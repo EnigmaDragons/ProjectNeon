366a0c99a8779e02a2aaa5c67ec1b9d8aa654fe2
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-12 14:33:32 -0300

    Closes #25: Reviews corrections [2]
    - removed NoCharacter c# file
    - added party script

---
+++ src/ProjectNeon/Assets/SquadSelection/Party.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Party : ScriptableObject
+{
+    private Character characterOne = new NoCharacter();
+    private Character characterTwo;
+    private Character characterThree;
+
+    public Party( Character one, Character two, Character three)
+    {
+        this.characterOne = one;
+        this.characterTwo = two;
+        this.characterThree = three;
+    }
+
+}
