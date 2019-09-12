0df1705d49578c401ce105ae98d961c686b3d777
Silas Reinagel <silas.reinagel@carvana.com>
2019-09-11 18:10:32 -0700

    Setup Simple Reactive Event Scriptable Objects. Closes #53

---
+++ src/ProjectNeon/Assets/Battle/BattleOrchestrator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public sealed class BattleOrchestrator : MonoBehaviour
+{
+    [SerializeField] private GameEvent OnStart;
+    [SerializeField] private GameEventListener OnSetupBegun;
+    [SerializeField] private string CurrentPhase = "Not Initialized";
+    
+    void Start()
+    {
+        OnStart.Publish();
+    }
+
+    public void SetCurrentPhaseDescription(string description) => CurrentPhase = description;
+}
