324bc9c778cdac1b7da4c8615b3c1878efd42d20
Paulo Lobo <33915433+paulodamaso@users.noreply.github.com>
2019-09-13 01:25:41 -0300

    For #25: Creation of Character and NoCharacter classes (#50)
    * For #25: Creation of Character and NoCharacter classes
    
    * Closes #25: Review corrections
    
    * Closes #25: Reviews corrections [2]
    
    - removed NoCharacter c# file
    - added party script
    
    * Closes #25: Added party scriptableobjet with serialized fields and NoCharacter as first character

---
+++ src/ProjectNeon/Assets/SquadSelection/Party.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Party : ScriptableObject
+{
+    [SerializeField]
+    private Character characterOne;
+
+    [SerializeField]
+    private Character characterTwo;
+
+    [SerializeField]
+    private Character characterThree;
+
+}
