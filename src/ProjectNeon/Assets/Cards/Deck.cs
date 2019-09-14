9c66c3d6bb8c5184a1500e9840eae0f28811361c
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-14 02:04:56 -0300

    For #27: Created Deck and Card classes, and shuffle() method for Deck class

---
+++ src/ProjectNeon/Assets/Cards/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck 
+{
+    private List<Card> cards;
+
+    Deck(List<Card> cards)
+    {
+        this.cards = cards;
+    }
+
+    void shuffle()
+    {
+        cards.Sort((a, b) => (int)System.Math.Round(Random.value, 0));
+    }
+
+
+}
