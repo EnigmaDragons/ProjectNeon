2f93bcef612ed644006693171a000dc0f92d884d
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-13 17:23:57 -0700

    Added Squad Selection cycling. Closes #65 (#69)

---
+++ src/ProjectNeon/Assets/Characters/Character.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class Character : ScriptableObject
+{
+    [SerializeField] private Sprite Bust;
+    [SerializeField] private string ClassName;
+}
