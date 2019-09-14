ff2ad3563624ad6dcd6568d496857b8c0684c0d9
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-14 01:40:24 -0300

    Closes #59: Created Navigator class and added navigation to all scene buttons

---
+++ src/ProjectNeon/Assets/Scripts/Navigator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Navigate(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
