6b2bb67fb1dfe5a088266dde208e8be59371c457
SilasReinagel <silas.reinagel@gmail.com>
2019-09-14 22:55:20 -0700

    Created Battle Hand Display Visualizer. Closes #30

---
+++ src/ProjectNeon/Assets/Battle/Debug/DebugDrawCard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DebugDrawCard : MonoBehaviour
+{
+    [SerializeField] private CardPlayZone Deck;
+    [SerializeField] private CardPlayZone Hand;
+
+    public void DrawOneCard()
+    {
+        Hand.PutOnBottom(Deck.DrawOneCard());
+    }
+}
