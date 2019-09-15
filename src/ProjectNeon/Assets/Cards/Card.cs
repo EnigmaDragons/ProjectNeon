567953d6f5fe3fbb11d1bd163c021a3765b84c78
SilasReinagel <silas.reinagel@gmail.com>
2019-09-14 23:49:45 -0700

    Created Placeholder card. Closes #31

--- src/ProjectNeon/Assets/Cards/Card.cs
+++ src/ProjectNeon/Assets/Cards/Card.cs
@@ -3,5 +3,8 @@
 [CreateAssetMenu()]
 public class Card : ScriptableObject
 {
-    
+    [SerializeField] private Sprite Art;
+    [SerializeField] private CardEffect Effect;
+    [SerializeField] private string Description;
+    [SerializeField] private string TypeDescription;
 }
