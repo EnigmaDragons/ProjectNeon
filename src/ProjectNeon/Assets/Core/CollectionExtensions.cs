dd78a514762c793793c0896175918c606759b13c
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-14 22:56:08 -0700

    Created Battle Hand Display Visualizer. Closes #30 (#76)

--- src/ProjectNeon/Assets/Core/CollectionExtensions.cs
+++ src/ProjectNeon/Assets/Core/CollectionExtensions.cs
@@ -1,14 +1,14 @@
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    public static class CollectionExtensions
-    {
-        public static T[] AsArray<T>(this T item) => new [] {item};
-        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) => items.ToList().ForEach(action);
-        public static IEnumerable<T> Concat<T>(this IEnumerable<T> items, T item) => items.Concat(item.AsArray());
-        public static IEnumerable<T> Except<T>(this IEnumerable<T> items, T item) => items.Except(item.AsArray());
-        public static bool None<T>(this IEnumerable<T> items) => !items.Any();
-        public static IEnumerable<T> WrappedWith<T>(this IEnumerable<T> items, T wrapping) => wrapping.AsArray().Concat(items).Concat(wrapping);
-
-    }
+public static class CollectionExtensions
+{
+    public static T[] AsArray<T>(this T item) => new [] {item};
+    public static void ForEach<T>(this IEnumerable<T> items, Action<T> action) => items.ToList().ForEach(action);
+    public static IEnumerable<T> Concat<T>(this T item, IEnumerable<T> items)  => item.AsArray().Concat(items);
+    public static IEnumerable<T> Concat<T>(this IEnumerable<T> items, T item) => items.Concat(item.AsArray());
+    public static IEnumerable<T> Except<T>(this IEnumerable<T> items, T item) => items.Except(item.AsArray());
+    public static bool None<T>(this IEnumerable<T> items) => !items.Any();
+    public static IEnumerable<T> WrappedWith<T>(this IEnumerable<T> items, T wrapping) => wrapping.AsArray().Concat(items).Concat(wrapping);
+}
