6a4825559f3c3f0f047ca1565f8a1394eac9a88e
Paulo Lobo <33915433+paulodamaso@users.noreply.github.com>
2019-09-15 19:09:11 -0300

    For #27: Created Deck and Card classes, and shuffle() method for Deck… (#75)
    * For #27: Created Deck and Card classes, and shuffle() method for Deck class
    
    * Closes #27: Review changes [1]
    
    * Closes #27: Review changes [2]
    
    * Closes #27: Review changes [3]
    
    - Fixed start method
    - Added serialized fields to script
    - Created test case

---
+++ src/ProjectNeon/Assets/Cards/BattleSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleSetup : MonoBehaviour
+{
+    [SerializeField]
+    Deck player;
+
+    [SerializeField]
+    List<Deck> enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.Shuffle();
+        foreach(Deck deck in enemy){
+            deck.Shuffle();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
