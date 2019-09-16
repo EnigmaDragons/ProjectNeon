5958634d433f01959dd0adf736f51f4cd89da285
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-16 18:21:29 -0300

    For #51: Added Enemy Scriptable Objects
    - Created Enemy, Stats and TurnAI Scriptted objects
    - Created an Enemy with Test components.

---
+++ src/ProjectNeon/Assets/Enemies/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Enemy : ScriptableObject
+{
+    [SerializeField]
+    private string enemyName;
+
+    [SerializeField]
+    private Deck deck;
+
+    [SerializeField]
+    private TurnAI turn;
+
+    [SerializeField]
+    private Stats stats;
+
+}
---
+++ src/ProjectNeon/Assets/Enemies/Stats.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class Stats : ScriptableObject
+{
+
+}
