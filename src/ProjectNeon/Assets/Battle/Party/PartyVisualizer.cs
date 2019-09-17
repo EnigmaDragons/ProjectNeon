0b5418717c2d3b1c9da6d8b402badcaa92e0d45a
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-17 00:07:13 -0300

    Closes #28: added puzzle and renamed variables

--- src/ProjectNeon/Assets/Battle/Party/PartyVisualizer.cs
+++ src/ProjectNeon/Assets/Battle/Party/PartyVisualizer.cs
@@ -6,15 +6,18 @@ public class PartyVisualizer : MonoBehaviour
 {
 
     [SerializeField] private PartyArea partyArea;
-    [SerializeField] private GameObject _character1;
-    [SerializeField] private GameObject _character2;
+    [SerializeField] private GameObject character1;
+    [SerializeField] private GameObject character2;
     [SerializeField] private GameObject character3;
 
+    /**
+     * @todo #28:15min Render player characters in battle screen.
+     */
 
     void Start()
     {
-        _character1.GetComponent<SpriteRenderer>().sprite = partyArea.party.characterOne.Bust;
-        _character2.GetComponent<SpriteRenderer>().sprite = partyArea.party.characterTwo.Bust;
+        character1.GetComponent<SpriteRenderer>().sprite = partyArea.party.characterOne.Bust;
+        character2.GetComponent<SpriteRenderer>().sprite = partyArea.party.characterTwo.Bust;
         character3.GetComponent<SpriteRenderer>().sprite = partyArea.party.characterThree.Bust;
     }
 }
