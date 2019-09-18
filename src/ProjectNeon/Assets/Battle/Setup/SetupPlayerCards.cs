7e3f0ec2844758de421bafe0404a716daefe31e1
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-17 22:21:55 -0700

    Added Int/Float/String Variables and References. Closes #81 (#123)

--- src/ProjectNeon/Assets/Battle/Setup/SetupPlayerCards.cs
+++ src/ProjectNeon/Assets/Battle/Setup/SetupPlayerCards.cs
@@ -2,16 +2,15 @@
 
 public class SetupPlayerCards : MonoBehaviour
 {
-    [SerializeField] private CardPlayZones PlayerCardPlayZones;
-    [SerializeField] private Card DebugDefaultCard;
-    [SerializeField] private GameEvent OnFinished;
+    [SerializeField] private CardPlayZones playerCardPlayZones;
+    [SerializeField] private Card debugDefaultCard;
+    [SerializeField] private GameEvent onFinished;
+    [SerializeField] private IntReference startingCards;
 
-    // @todo #30:10min Create IntVariable Scriptable Object and setup StartingCards IntVariable
-
-    private CardPlayZone Hand => PlayerCardPlayZones.HandZone;
-    private CardPlayZone Discard => PlayerCardPlayZones.DiscardZone;
-    private CardPlayZone Play => PlayerCardPlayZones.PlayZone;
-    private CardPlayZone Deck => PlayerCardPlayZones.DrawZone;
+    private CardPlayZone Hand => playerCardPlayZones.HandZone;
+    private CardPlayZone Discard => playerCardPlayZones.DiscardZone;
+    private CardPlayZone Play => playerCardPlayZones.PlayZone;
+    private CardPlayZone Deck => playerCardPlayZones.DrawZone;
 
     void Awake()
     {
@@ -20,14 +19,14 @@ public class SetupPlayerCards : MonoBehaviour
         Play.Clear();
         Deck.Clear();
         for (var i = 0; i < 8; i++)
-            Deck.PutOnBottom(DebugDefaultCard);
+            Deck.PutOnBottom(debugDefaultCard);
     }
 
     void Start()
     {
         Deck.Shuffle();
-        for (var c = 0; c < 6; c++)
+        for (var c = 0; c < startingCards.Value; c++)
             Hand.PutOnBottom(Deck.DrawOneCard());
-        OnFinished.Publish();
+        onFinished.Publish();
     }
 }
---
+++ src/ProjectNeon/Assets/Core/Variables/IntReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+[Serializable]
+public class IntReference
+{
+    public bool UseConstant = true;
+    public int ConstantValue;
+    public IntVariable IntVariable;
+
+    public int Value => UseConstant ? ConstantValue : IntVariable.Value;
+
+    public IntReference() { }
+
+    public IntReference(int value)
+    {
+        UseConstant = true;
+        ConstantValue = value;
+    }
+
+    public static implicit operator int(IntReference reference)
+    {
+        return reference.Value;
+    }
+}
