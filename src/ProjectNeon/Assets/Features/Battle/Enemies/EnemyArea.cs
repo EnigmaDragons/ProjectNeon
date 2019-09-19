fc71e6d16eac8d6d68e266b281f65af1b432c183
SilasReinagel <silas.reinagel@gmail.com>
2019-09-19 00:43:57 -0700

    Cleaned up Project Organization

---
+++ src/ProjectNeon/Assets/Features/Battle/Enemies/EnemyArea.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class EnemyArea : ScriptableObject
+{
+    [SerializeField] public List<Enemy> enemies;
+}
---
+++ src/ProjectNeon/Assets/Features/Battle/Enemies/EnemyVisualizer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/**
+ * Puts the enemy party into screen.
+ * 
+ * Display enemies into screen in the following format (each number is enemies variable index):
+ * 0 2 4 6
+ *  1 3 5
+ * Uses enemy sprites with default size of 100 x 200
+ */
+public class EnemyVisualizer : MonoBehaviour
+{
+    [SerializeField] private EnemyArea enemyArea;
+    [SerializeField] private GameObject enemyPrototype;
+    [SerializeField] private GameEvent onSetupFinished;
+    [SerializeField] private float rowHeight = 1.5f;
+    [SerializeField] private float widthBetweenEnemies = 1.5f; 
+
+    public void SetupEnemies()
+    {
+        for (var i= 0; i < enemyArea.enemies.Count; i++)
+        {
+            var enemy = Instantiate(enemyPrototype, transform);
+            var r = enemy.AddComponent<SpriteRenderer>();
+            r.sprite = enemyArea.enemies[i].image;
+            r.transform.position = transform.position + new Vector3(i * widthBetweenEnemies, (i % 2) * rowHeight, 0);
+        }
+        onSetupFinished.Publish();
+    }
+}
