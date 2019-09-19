cfecd225e12b7b48b310f9c7379c4100b38fb223
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-19 00:44:35 -0700

    Cleaned up Project Organization (#134)

--- src/ProjectNeon/Assets/Battle/Enemies/EnemyArea.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class EnemyArea : ScriptableObject
-{
-    [SerializeField] public List<Enemy> enemies;
-}
---
+++ src/ProjectNeon/Assets/Data/Templates/Deck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Deck : ScriptableObject
+{
+    [SerializeField]
+    private List<Card> cards;
+
+    public Deck(List<Card> cards)
+    {
+        this.cards = cards;
+    }
+
+    public void Shuffle()
+    {
+    }
+
+}
