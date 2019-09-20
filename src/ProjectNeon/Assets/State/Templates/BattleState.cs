a7a74820766f281c121ea13fcda838bddb64e803
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-19 22:52:51 -0700

    BattleState is correctly setup by EncounterBuilder. Closes #141 (#154)

--- src/ProjectNeon/Assets/State/Templates/BattleState.cs
+++ src/ProjectNeon/Assets/State/Templates/BattleState.cs
@@ -3,9 +3,7 @@
 public class BattleState : ScriptableObject
 {
     [SerializeField] private Party party;
-
-    // @todo #1:10min Initialize this when the Encounter is Built
-    [SerializeField] private Enemy[] enemies;
+    [SerializeField] private EnemyArea enemies;
     
     // @todo #1:10min Replace this with Member once implemented.
     [SerializeField] private Enemy tempTarget;
