812088520311dd081155fc6381af342c27daf830
SilasReinagel <silas.reinagel@gmail.com>
2019-09-19 22:44:00 -0700

    Squad Selection uses player's Library. Closes #144

--- src/ProjectNeon/Assets/State/Templates/Library.cs
+++ src/ProjectNeon/Assets/State/Templates/Library.cs
@@ -2,8 +2,8 @@
 
 public class Library : ScriptableObject
 {
-    // @todo #1:10min Update Squad Selection to use Unlocked Characters as it's available character pool
-
     [SerializeField] private Character[] unlockedCharacters;
     [SerializeField] private Card[] unlockedCards;
+
+    public Character[] UnlockedCharacters => unlockedCharacters;    
 }
