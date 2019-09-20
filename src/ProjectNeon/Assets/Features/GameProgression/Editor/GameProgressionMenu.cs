76ba10e0cc7caefa416b542caf9f4b10a2555c1c
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-20 00:01:48 -0700

    Created Stage definition with elements. Closes #157 (#158)

---
+++ src/ProjectNeon/Assets/Features/GameProgression/Editor/GameProgressionMenu.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+public static class GameProgressionMenu
+{
+    [MenuItem("Assets/Create/GameContent/Stage")]
+    static void Stage() => Create<Stage>();
+    [MenuItem("Assets/Create/GameContent/FixedEncounter")]
+    static void FixedEncounter() => Create<SpecificEncounterSegment>();
+
+    private static void Create<T>() where T : ScriptableObject
+    {
+        var asset = ScriptableObject.CreateInstance<T>();
+        var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        path += $"/New{typeof(T).Name}.asset";
+        ProjectWindowUtil.CreateAsset(asset, path);
+    }
+}
---
+++ src/ProjectNeon/Assets/Features/GameProgression/SpecificEncounterSegment.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+class SpecificEncounterSegment : StageSegment
+{
+    [SerializeField] private Enemy[] enemies;
+
+    public override string Name => "Battle";
+    
+    public override void Start()
+    {
+        // @todo #1:30min Setup Custom Encounter, instead of letting Battle Scene randomly generate one
+        SceneManager.LoadScene("BattleScene");
+    }
+}
