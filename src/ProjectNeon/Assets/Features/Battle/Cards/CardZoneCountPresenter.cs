e29a03aee8778a3ea95c7b6cc941129cec28e5e9
SilasReinagel <silas.reinagel@gmail.com>
2019-09-21 19:27:03 -0700

    Added Deck Counter. Closes #171

---
+++ src/ProjectNeon/Assets/Features/Battle/Cards/CardZoneCountPresenter.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class CardZoneCountPresenter : MonoBehaviour
+{
+    [SerializeField] private CardPlayZone zone;
+    [SerializeField] private TextMeshProUGUI textField;
+
+    private void OnEnable()
+    {
+        zone.OnZoneCardsChanged.Subscribe(UpdateCardCount, this);
+    }
+
+    private void OnDisable()
+    {
+        zone.OnZoneCardsChanged.Unsubscribe(this);
+    }
+
+    private void UpdateCardCount() => textField.text = zone.Cards.Length.ToString();
+}
