34f0589c5250b64afec82b236a4e9e68ba81bc1b
SilasReinagel <silas.reinagel@gmail.com>
2019-09-21 20:24:58 -0700

    Created Shop Stage. Incorporated into Adventure flow. Closes #173

---
+++ src/ProjectNeon/Assets/Features/GameProgression/ShopSegment.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+class ShopSegment : StageSegment
+{
+    public override string Name => "Shop";
+
+    public override void Start() => SceneManager.LoadScene("ShopScene");
+}
