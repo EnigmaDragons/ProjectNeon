645a7b7feeca6ec64bb00b0e940ee7f9819c53c0
SilasReinagel <silas.reinagel@gmail.com>
2019-09-23 23:19:55 -0700

    Setup Stats for characters. Closes #181

---
+++ src/ProjectNeon/Assets/Data/Templates/ResourceType.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public abstract class ResourceType : ScriptableObject, IResourceType
+{
+    public abstract string Name { get; }
+    public abstract int MaxAmount { get; }
+}
--- src/ProjectNeon/Assets/Data/Templates/Stats.cs
+++ src/ProjectNeon/Assets/Data/Templates/Stats.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 
-public class Stats : ScriptableObject
+public abstract class Stats : ScriptableObject, IStats
 {
-
+    public abstract int MaxHP { get; }
+    public abstract int MaxShield { get; }
+    public abstract int Attack { get; }
+    public abstract int Magic { get; }
+    public abstract float Armor { get; }
+    public abstract float Resistance { get; }
+    public abstract IResourceType[] ResourceTypes { get; }
 }
