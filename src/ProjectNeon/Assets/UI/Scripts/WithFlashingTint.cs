2bad75d497bf64ea20d89e704ea1ac2ad68f59f4
Silas Reinagel <silas.reinagel@carvana.com>
2019-09-29 18:20:37 -0700

    Created Flashing Selection UI indicator. Closes #149

---
+++ src/ProjectNeon/Assets/UI/Scripts/WithFlashingTint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WithFlashingTint : MonoBehaviour
+{
+    [SerializeField] private float amount = 0.3f;
+    [SerializeField] private float duration = 0.75f;
+
+    private readonly float _originalAmount = 1.0f;
+    private readonly Color _original = Color.white;
+    private Color _darker;
+    private Image _sprite;
+
+    private void Start()
+    {
+        _sprite = GetComponent<Image>();
+        _darker = new Color(_originalAmount - amount, _originalAmount - amount, _originalAmount - amount, _sprite.color.a);
+    }
+
+    void Update()
+    {
+        _sprite.color = Color.Lerp(_original, _darker, Mathf.PingPong(Time.time, duration));
+    }
+}
