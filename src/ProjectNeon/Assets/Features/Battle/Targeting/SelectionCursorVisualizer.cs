dda21545d1742249732bd9c3aad82dc33dc63119
Silas Reinagel <silas.reinagel@gmail.com>
2019-09-29 21:06:52 -0700

    Show/Hide Selection Cursor. Closes #236 (#237)

---
+++ src/ProjectNeon/Assets/Features/Battle/Targeting/SelectionCursorVisualizer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SelectionCursorVisualizer : MonoBehaviour
+{
+    [SerializeField] private GameEvent activateOn;
+    [SerializeField] private GameEvent deactivateOn;
+    [SerializeField] private GameObject cursor;
+    [SerializeField] private BattlePlayerTargetingState targeting;
+
+    private void OnEnable()
+    {
+        activateOn.Subscribe(() => cursor.SetActive(true), this);
+        deactivateOn.Subscribe(() => cursor.SetActive(false), this);
+    }
+
+    private void OnDisable()
+    {
+        activateOn.Unsubscribe(this);
+        deactivateOn.Unsubscribe(this);
+    }
+}
