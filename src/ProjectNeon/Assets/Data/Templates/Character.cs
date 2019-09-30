ca1e8b72d39163534b4fd785e1e7ae5028861d3f
paulodamaso <pauloeduardolobo@gmail.com>
2019-09-30 12:47:55 -0300

    For #216: Decklist displays current hero deck
    - Added DeckListViewCards script which will get all hero cards from party and put them into DeckList
    - Added List<Card > to Hero so we can get that hero specific set of cards

--- src/ProjectNeon/Assets/Data/Templates/Character.cs
+++ src/ProjectNeon/Assets/Data/Templates/Character.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Character : ScriptableObject
 {
     public Sprite Bust;
     public Stats Stats;
     public string ClassName;
+
+    [SerializeField]
+    private List<Card> cards;
+
+    public List<Card> Cards => cards;
 }
