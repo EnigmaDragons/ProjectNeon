13fc509185e58d7c9a5a76b2a32eb29c7af0cb4d
Silas Reinagel <silas.reinagel@carvana.com>
2019-09-30 21:52:28 -0700

    Cards have Class Limitations using StringVariables. Closes #233

--- src/ProjectNeon/Assets/Data/Templates/Card.cs
+++ src/ProjectNeon/Assets/Data/Templates/Card.cs
@@ -6,14 +6,13 @@ public class Card : ScriptableObject
     [SerializeField] private Sprite art;
     [SerializeField] private string description;
     [SerializeField] private string typeDescription;
-    // @todo #1:15min Limit this by Class. Create StringVariable for all classes to reduce fragility
-    [SerializeField] private string onlyPlayableByHeroName;
+    [SerializeField] private StringVariable onlyPlayableByClass;
     [SerializeField] private CardAction[] actions;
 
     public Sprite Art => art;
     public string Description => description;
     public string TypeDescription => typeDescription;
     public CardAction[] Actions => actions.ToArray();
-    public Maybe<string> LimitedToHero => new Maybe<string>(onlyPlayableByHeroName);
+    public Maybe<string> LimitedToClass => new Maybe<string>(onlyPlayableByClass.Value.Length > 0 ? onlyPlayableByClass.Value : null);
 
 }
