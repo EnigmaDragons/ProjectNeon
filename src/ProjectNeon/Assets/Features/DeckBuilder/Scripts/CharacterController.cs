f343782f51614076a8b016e1cd42a9cb8b00e116
WhimsySoftware <52692144+WhimsySoftware@users.noreply.github.com>
2019-10-01 06:37:00 +0300

    Add characters to DeckBuilder. Closes #142 (#139) (#203)
    * Add characters to DeckBuilder. Closes #142 (#139)
    
    * Add scene ui changes. Closes #142 (#139)
    
    * Added new element as Prefab

---
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public static CharactersEnum currentCharacter = CharactersEnum.Character1;
+
+    public delegate void ChangeCurrentCharacter();
+    public event ChangeCurrentCharacter OnCharacterChanged;
+
+    public static CharacterController instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+    public void SelectCharacter(CharactersEnum characterValue)
+    {
+        currentCharacter = characterValue;
+        if (OnCharacterChanged != null)
+            OnCharacterChanged.Invoke();
+    }
+}
---
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/CharecterIcon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharecterIcon : MonoBehaviour
+{
+    [SerializeField, ReadOnly] bool selected;
+    [SerializeField] CharactersEnum characterValue;
+    [SerializeField] GameObject selectImage;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+        CharacterController.instance.OnCharacterChanged += OnCharacterChanged;
+        OnCharacterChanged();
+    }
+
+    private void OnCharacterChanged()
+    {
+        selected = CharacterController.currentCharacter == characterValue;
+        selectImage.SetActive(selected);
+    }
+
+    public void OnClick()
+    {
+        CharacterController.instance.SelectCharacter(characterValue);
+    }
+}
