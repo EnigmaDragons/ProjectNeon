e2a07b7707a6091f4ec8def13d3547b8471ac61f
paulodamaso <pauloeduardolobo@gmail.com>
2019-10-01 11:23:43 -0300

    For #216:
    - Added `DeckController` to `DeckListView`: this script will make Deck adjustments in this screen
    - Added `PartyDeck` reference to `DeckController` and created OnHeroChange method, to be executed when Hero is changed on Character controller

---
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/DeckController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CharacterController;
+
+public class DeckController : MonoBehaviour
+{
+    [SerializeField] private PartyDecks decks;
+
+    private Deck current;
+
+    public void OnHeroChange()
+    {
+        this.current = decks.Decks[
+            Array.IndexOf(
+                Enum.GetValues(CharacterController.currentCharacter.GetType()),
+                CharacterController.currentCharacter)
+            ];
+    }
+
+
+}
--- src/ProjectNeon/Assets/State/Templates/PartyDecks.cs
+++ src/ProjectNeon/Assets/State/Templates/PartyDecks.cs
@@ -3,4 +3,6 @@
 public class PartyDecks : ScriptableObject
 {
     [SerializeField] private Deck[] decks;
+
+    public Deck[] Decks => decks;
 }
