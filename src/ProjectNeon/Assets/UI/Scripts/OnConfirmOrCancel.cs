6940942b51467a0d1c6f2527dac968dc69cca05e
Silas Reinagel <silas.reinagel@carvana.com>
2019-10-01 17:47:41 -0700

    Prevents Keyboard Hand Card Selection during Targeting. Closes #250

--- src/ProjectNeon/Assets/UI/Scripts/OnConfirmOrCancel.cs
+++ src/ProjectNeon/Assets/UI/Scripts/OnConfirmOrCancel.cs
@@ -3,30 +3,11 @@
 
     public sealed class OnConfirmOrCancel : MonoBehaviour
     {
-        [ReadOnly, SerializeField] private bool isActive;
-        [SerializeField] private GameEvent activateOn;
-        [SerializeField] private GameEvent deactivateOn;
         [SerializeField] private UnityEvent onSubmit;
         [SerializeField] private UnityEvent onCancel;
         
-        private void OnEnable()
-        {
-            isActive = false;
-            activateOn.Subscribe(() => isActive = true, this);
-            deactivateOn.Subscribe(() => isActive = false, this);
-        }
-
-        private void OnDisable()
-        {
-            activateOn.Unsubscribe(this);
-            deactivateOn.Unsubscribe(this);
-        }
-
         private void Update()
         {
-            if (!isActive)
-                return;
-
             if (Input.GetButtonDown("Submit"))
                 onSubmit.Invoke();
             if (Input.GetButtonDown("Cancel"))
--- src/ProjectNeon/Assets/UI/Scripts/OnDirectionInput.cs
+++ src/ProjectNeon/Assets/UI/Scripts/OnDirectionInput.cs
@@ -4,9 +4,6 @@ using UnityEngine.Events;
 
 public class OnDirectionInput : MonoBehaviour
 {
-    [ReadOnly, SerializeField] private bool isActive;
-    [SerializeField] private GameEvent activateOn;
-    [SerializeField] private GameEvent deactivateOn;
     [SerializeField] private UnityEvent onLeft;
     [SerializeField] private UnityEvent onRight;
     [SerializeField] private UnityEvent onUp;
@@ -14,25 +11,9 @@ public class OnDirectionInput : MonoBehaviour
 
     private float _lastHDir = 0;
     private float _lastVDir = 0;
-    
-    private void OnEnable()
-    {
-        isActive = false;
-        activateOn.Subscribe(() => isActive = true, this);
-        deactivateOn.Subscribe(() => isActive = false, this);
-    }
-
-    private void OnDisable()
-    {
-        activateOn.Unsubscribe(this);
-        deactivateOn.Unsubscribe(this);
-    }
 
     void Update()
     {
-        if (!isActive)
-            return;
-
         var hDir = Input.GetAxisRaw("Horizontal");
         if (Math.Abs(hDir - _lastHDir) > 0.1f)
         {
