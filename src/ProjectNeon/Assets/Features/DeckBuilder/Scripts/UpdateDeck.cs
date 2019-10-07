eeb0af0234e8f6d25839e1c3a9bb1ceb4d07cd51
paulodamaso <pauloeduardolobo@gmail.com>
2019-10-07 18:59:44 -0300

    For #282: Initial implementation

---
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/UpdateDeck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateLibrary : MonoBehaviour
+{
+    [SerializeField] private Library library;
+
+    public Card[] select(Hero hero)
+    {
+        List<Card> cards = new List<Card>(0);
+        library.UnlockedCards.ForEach(
+            card =>
+            {
+                if (
+                    card.LimitedToClass.IsPresent 
+                )
+                {
+                    cards.Add(card);
+                }
+            }
+        );
+        return cards.ToArray();
+    }
+
+}
