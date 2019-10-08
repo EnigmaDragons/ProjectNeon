8912887d2b9f8d754bb5da128867f2cbf6719d01
paulodamaso <pauloeduardolobo@gmail.com>
2019-10-08 18:00:45 -0300

    For #283: Clicking on Card on list view should put it on main pane, and vice-versa

---
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/CardSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSelector : MonoBehaviour
+{
+    [SerializeField] private DeckBuilderState state;
+    [SerializeField] private Card current;
+
+    public void ListToDeck()
+    {
+        this.state.ListToDeck(current);
+    }
+
+    public void DeckToList()
+    {
+        this.state.DeckToList(current);
+    }
+}
--- src/ProjectNeon/Assets/State/Templates/DeckBuilderState.cs
+++ src/ProjectNeon/Assets/State/Templates/DeckBuilderState.cs
@@ -19,6 +19,26 @@ public class DeckBuilderState : ScriptableObject
         return this.decks.Decks[0];
     }
 
+    public void ListToDeck(Card card)
+    {
+        this.decks.Decks[0].Cards.Remove(card);
+        this.current.Cards.Add(card);
+        onCurrentDeckChanged.Publish();
+        /**
+         * @todo #216:30min We need to discover which deck belongs to which hero, so we can
+         *  select the correct deck to remove or add the cards to. Maybe PartyDecks must be refactored
+         *  to a MAP like structure with the heroes as keys. Then fix both ListToDeck and DeckToList
+         *  methods to call the correct deck.
+         */
+    }
+
+    public void DeckToList(Card card)
+    {
+        this.current.Cards.Remove(card);
+        this.decks.Decks[0].Cards.Add(card);
+        onCurrentDeckChanged.Publish();
+    }
+
     /**
      * @todo #216:30min We need to correctly initialize and update current Deck. This deck should
      *  change each time we change character in DeckBuilderUi (ChangeCurrentCharacter Event).
