6da0767770f96d048e4df424d21e47e6aae38902
paulodamaso <pauloeduardolobo@gmail.com>
2019-10-10 17:26:44 -0300

    For #289: Correcting test

--- src/ProjectNeon/Assets/Z_Tests/Features/Battle/StatsDecorationTests.cs
+++ src/ProjectNeon/Assets/Z_Tests/Features/Battle/StatsDecorationTests.cs
@@ -6,8 +6,8 @@ public class StatsDecoRationTests
     [Test]
     public void Stats_Barkskin_DecorateArmor()
     {
-        var memberStats = TestableObjectFactory.Create<MemberState>();
-        var spellStats = TestableObjectFactory.Create<BarkskinStats>();
+        MemberState memberStats = TestableObjectFactory.Create<MemberState>();
+        BattleStats spellStats = TestableObjectFactory.Create<BarkskinStats>();
         spellStats.Init(memberStats);
         memberStats.CurrentStats = spellStats;
         Assert.IsTrue(spellStats.Armor.Equals(10.0F));
