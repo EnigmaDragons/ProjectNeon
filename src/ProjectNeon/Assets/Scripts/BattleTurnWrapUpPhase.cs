d4aa712fa834d473f1a5e25b7b71ac3984a32644
talioo1997 <talioo1997@gmail.com>
2019-10-10 18:10:31 +0300

    Add BattleTurnWrapUpPhase. Closes #311

---
+++ src/ProjectNeon/Assets/Scripts/BattleTurnWrapUpPhase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleTurnWrapUpPhase : MonoBehaviour
+{
+    [SerializeField] private GameEvent battleTurnWrapUpStarted;
+    [SerializeField] private GameEvent battleTurnWrapUpFinished;
+    [SerializeField] private GameEvent playerTurnConfirmed;
+    [SerializeField] private GameEvent turnStarted;
+
+    private void OnEnable()
+    {
+        playerTurnConfirmed.Subscribe(StartBattleTurnPhase, this);
+    }
+    void StartBattleTurnPhase()
+    {
+        battleTurnWrapUpStarted.Publish();
+        print("StartPhase");
+    }
+    [ContextMenu("Finish phase")]
+    void FinishBattleTurnPhase()
+    {
+        battleTurnWrapUpFinished.Publish();
+        // @todo #311: 30min Add re-setup battle cards
+        turnStarted.Publish();
+    }
+}
