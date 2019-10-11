94884dfc5b3ee01713213c7cc1175225bc21a1a9
Silas Reinagel <silas.reinagel@carvana.com>
2019-10-10 17:54:56 -0700

    Added 2 Target Dummies, to ease Battle Testing.

--- src/ProjectNeon/Assets/Data/Templates/Hero.cs
+++ src/ProjectNeon/Assets/Data/Templates/Hero.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using Features.Combatants;
+using UnityEngine;
 
 public class Hero : ScriptableObject
 {
     [SerializeField] private Sprite bust;
-    [SerializeField] private Stats stats;
     [SerializeField] private StringVariable className;
-
+    
+    // Stats
+    [SerializeField] private int maxHp;
+    [SerializeField] private int attack;
+    [SerializeField] private int magic;
+    [SerializeField] private float armor;
+    [SerializeField] private float resistance;
+    [SerializeField] private ResourceType resource1;
+    
     public Sprite Bust => bust;
-    public Stats Stats => stats;
     public StringVariable ClassName => className;
+    
+    public IStats Stats => new InMemoryStats
+    {
+        MaxHP = maxHp,
+        Magic = magic,
+        Attack = attack,
+        Armor = armor,
+        Resistance = resistance,
+        MaxShield = (int)Math.Ceiling(maxHp / 3m),
+        ResourceTypes = resource1.AsArray(),
+    };
 }
---
+++ src/ProjectNeon/Assets/Features/Combatants/InMemoryStats.cs
@@ -0,0 +1,13 @@
+namespace Features.Combatants
+{
+    public sealed class InMemoryStats : IStats
+    {
+        public int MaxHP { get; set; }
+        public int MaxShield { get; set; }
+        public int Attack { get; set; }
+        public int Magic { get; set;  }
+        public float Armor { get; set; }
+        public float Resistance { get; set; }
+        public IResourceType[] ResourceTypes { get; set; } = new IResourceType[0];
+    }
+}
