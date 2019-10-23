ae3f34c8e6bc1c85a59ed86ee517135c3a02e3db
Silas Reinagel <silas.reinagel@gmail.com>
2019-10-22 17:50:27 -0700

    Shows Resource Icons in the HUD. Closes #368 (#372)

--- src/ProjectNeon/Assets/Features/Battle/Party/HeroBattleUIPresenter.cs
+++ src/ProjectNeon/Assets/Features/Battle/Party/HeroBattleUIPresenter.cs
@@ -6,10 +6,26 @@ public class HeroBattleUIPresenter : MonoBehaviour
     [SerializeField] private Image bust;
     [SerializeField] private HPBarController hp;
     [SerializeField] private BattleState state;
+    [SerializeField] private ResourceCounterPresenter resource1;
+    [SerializeField] private ResourceCounterPresenter resource2;
     
     public void Set(Hero hero)
     {
         bust.sprite = hero.Bust;
         hp.Init((int)state.GetMemberByHero(hero).State.MaxHP());
+        InitResources(hero.Stats.ResourceTypes);
+    }
+
+    private void InitResources(IResourceType[] resources)
+    {
+        if (resources.Length > 0 && resources[0].MaxAmount > 0)
+            resource1.Init(resources[0]);
+        else
+            resource1.Hide();
+
+        if (resources.Length > 1 && resources[1].MaxAmount > 0)
+            resource2.Init(resources[1]);
+        else
+            resource2.Hide();
     }
 }
---
+++ src/ProjectNeon/Assets/Features/Battle/Party/ResourceCounterPresenter.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceCounterPresenter : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private TextMeshProUGUI counter;
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+    
+    public void Init(IResourceType resource)
+    {
+        icon.sprite = resource.Icon;
+        counter.text = $"{resource.StartingAmount}/{resource.MaxAmount}";
+    }
+}
