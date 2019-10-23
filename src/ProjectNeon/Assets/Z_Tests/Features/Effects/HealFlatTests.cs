03e4bfe24f7deb77ebb15d88bd21d044c4621b04
Paulo Lobo <33915433+paulodamaso@users.noreply.github.com>
2019-10-23 10:59:42 -0300

    For 361: created more 3 paladin cards (#376)
    *  For #361: Created ShieldToughness and ArmorFlat effects, added Protect, Mark of Salvation and Bulwark of the Light cards
    
    * For #361: Added tests for ArmorFlat, HealFlat and ShieldToughness effects
    
    * For #361: Review corrections (1)

---
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/HealFlatTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+
+public sealed class HealFlatTests
+{
+
+    private EffectData data = new EffectData { EffectType = EffectType.HealFlat, FloatAmount = new FloatReference(5) } ;
+
+    private Member performer = new Member(
+        1,
+        "Good Dummy One",
+        "Paladin",
+        TeamType.Party,
+        new StatAddends()
+    );
+
+    private Member target = new Member(
+        2,
+        "Good Dummy Two",
+        "Wooden Dummy",
+        TeamType.Party,
+        new StatAddends().With(StatType.MaxHP, 10).With(StatType.Damagability, 1)
+    );
+
+    [Test]
+    public void HealFlat_ApplyEffect_DoesNotPassFullHealth()
+    {
+        AllEffects.Apply(data, performer, new MemberAsTarget(target));
+        Assert.AreEqual(
+            10,
+            target.State[TemporalStatType.HP]
+        );
+    }
+
+    [Test]
+    public void HealFlat_ApplyEffect()
+    {
+        target.State.TakeRawDamage(6);
+        AllEffects.Apply(data, performer, new MemberAsTarget(target));
+        Assert.AreEqual(
+            9,
+            target.State[TemporalStatType.HP]
+        );
+    }
+}
---
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/ShieldToughnessTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class ShieldToughnessTests
+{
+
+    private EffectData data = new EffectData { EffectType = EffectType.ShieldToughness, FloatAmount = new FloatReference(1)} ;
+
+    private Member performer = new Member(
+        1, 
+        "Good Dummy One", 
+        "Paladin", 
+        TeamType.Party, 
+        new StatAddends().With(StatType.Toughness, 5)
+    );
+
+    private Member target = new Member(
+        2, 
+        "Good Dummy Two", 
+        "Wooden Dummy", 
+        TeamType.Party, 
+        new StatAddends()
+    );
+
+    [Test]
+    public void ShieldToughness_ApplyEffect()
+    {
+        AllEffects.Apply(data, performer, new MemberAsTarget(target));
+        Assert.AreEqual(
+            5,
+            target.State[TemporalStatType.Shield]
+        );
+    }
+}
