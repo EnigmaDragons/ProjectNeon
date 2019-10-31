233aa73a96d6b8806d2a06c3fa56e8e27fa148a6
paulodamaso <pauloeduardolobo@gmail.com>
2019-10-30 23:38:24 -0300

    For #378: Created StealLifeOnAttack effect and preliminary tests

---
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/StealLifeOnAttackTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+public sealed class StealLifeOnAttackTests
+{
+    private EffectData StealLifeOnAttack(float amount) => new EffectData {
+        EffectType = EffectType.StealLifeOnAttack,
+        FloatAmount = new FloatReference(amount)
+    }; 
+    
+    [Test]
+    public void ArmorFlat_ApplyEffect_ArmorIsChangedCorrectly()
+    {
+        Member caster = TestMembers.Any();
+        Member attacker = TestMembers.Create(
+            s => s.With(StatType.Attack, 1f).With(StatType.MaxHP, 10).With(StatType.Damagability, 1f)
+        );
+        Member target = TestMembers.Create(s => s.With(StatType.MaxHP, 10).With(StatType.Damagability, 1f));
+        attacker.State.TakeRawDamage(6);
+
+        AllEffects.Apply(StealLifeOnAttack(1), caster, new MemberAsTarget(attacker));
+        BattleEvent.Publish(new Attack(attacker, target));
+
+        Assert.AreEqual(5, attacker.State[TemporalStatType.HP]);
+    }
+}
