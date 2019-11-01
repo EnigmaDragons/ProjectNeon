77fd0ce0dbcfb3c8deb465e5b36397b8ced96f71
paulodamaso <pauloeduardolobo@gmail.com>
2019-11-01 14:52:32 -0300

    For #378: Leaving puzzle for event stack removal

--- src/ProjectNeon/Assets/Features/Effects/RecurrentEffect.cs
+++ src/ProjectNeon/Assets/Features/Effects/RecurrentEffect.cs
@@ -10,7 +10,6 @@ public sealed class RecurrentEffect :  Effect
         _effect = origin;
         _repetitions = times;
         _count = 0;
-
     }
 
     public void Apply(Member source, Target target)
@@ -20,5 +19,11 @@ public sealed class RecurrentEffect :  Effect
             _effect.Apply(source, target);
             _count++;
         }
+        /**
+         * @todo #378:30min Remove Effect from Effect stack once it reaches its repetition number.
+         *  Currently RecurrentEffect just does not remove effect from stack: it continues to be executed, 
+         *  but only the Aplly method is checked. An effect should be removed from the character once it expires
+         *  (i.e it was executed _repetition times)
+         */
     }
 }
