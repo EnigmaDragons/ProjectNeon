3da17d3034ccf7133f502ce4c4a5c9e59877701f
Paulo Lobo <33915433+paulodamaso@users.noreply.github.com>
2019-11-07 00:12:42 -0300

    For #381: Evade buff effects (#462)
    * Triggering effects skeleton
    
    * For #379: Correcting triggered effects
    
    * For #379:  Refining of TriggeredEffects concept
    
    * For #379: created OnAttackEffect and ShieldFlatOnAttackTest
    
    * For #379: wired effects on AuraOfRetribution card
    
    * For #349: fixed DamageAttackerOnAttackTests
    
    * For #379: review corrections
    
    * For #379: review fixes
    
    * For #379: review corrections on attack, effects on attack and tests
    
    * For #379: fixing AllEffects
    
    * For #378: Created StealLifeOnAttack effect and preliminary tests
    
    * For #378: created StealLife effect
    
    * For #378: Created RecurrentEffect decorator
    
    * For #378: Leaving puzzle for event stack removal
    
    * For #377:  Added InterceptAttack effect and tests
    
    * For #451: Added Timed and Recurrent decorators
    
    * For #377: review correction (1)
    
    * For #451: review corrections(1)
    
    * For #454: Implementing Attack concept
    
    * For #377: correcting riot shield effect
    
    * For #377: Corrected error
    
    * For #377: fixing implementation
    
    * For #377: fixing interception tests and logic
    
    * For #377: finishing attack interception
    
    * For #381: Evade buff effects
    
    * For #381: review corrections
    
    * For #381: review corrections (3)

---
+++ src/ProjectNeon/Assets/Features/Effects/Evade.cs
@@ -0,0 +1,23 @@
+﻿
+public class Evade : Effect
+{
+    private Target _effectTarget;
+
+    public void Apply(Member source, Target target)
+    {
+        _effectTarget = target;
+        BattleEvent.Subscribe<AttackToPerform>(attackToPerform => Execute(attackToPerform.Attack), this);
+    }
+
+    void Execute(Attack attack)
+    {
+        _effectTarget.Members.ForEach(
+            target => {
+                if (target.Equals(attack.Target.Members[0]))
+                {
+                    attack.Effect = new NoEffect();
+                }
+            }
+        );
+    }
+}
---
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/EvadeTests.cs
@@ -0,0 +1,24 @@
+﻿
+using NUnit.Framework;
+using UnityEngine;
+
+public sealed class EvadeTests
+{
+
+    [Test]
+    public void Evade_ApplyEffect_AttackIsEvaded()
+    {
+        Member evader = TestMembers.Create(s => s.With(StatType.MaxHP, 10).With(StatType.Damagability, 1f));
+        Member attacker = TestMembers.With(StatType.Attack, 1);
+
+        new Evade().Apply(evader, new MemberAsTarget(evader));
+
+        new Attack(5).Apply(attacker, evader);
+
+        Assert.AreEqual(
+            10,
+            evader.State[TemporalStatType.HP],
+            "Target did not evaded attack"
+        );
+    }
+}
