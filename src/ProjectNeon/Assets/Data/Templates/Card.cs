b758a1599f8eda838c866220fa25ccd36e34ba59
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-01 16:27:18 -0700

    Updated Card Data. Closes #551

--- src/ProjectNeon/Assets/Data/Templates/Card.cs
+++ src/ProjectNeon/Assets/Data/Templates/Card.cs
@@ -6,17 +6,17 @@ public class Card : ScriptableObject
 {
     [SerializeField] private Sprite art;
     [SerializeField] private string description;
-    [SerializeField] private string typeDescription;
+    [SerializeField] private StringVariable typeDescription;
     [SerializeField] private StringVariable onlyPlayableByClass;
     [SerializeField] private ResourceCost cost;
     [SerializeField] private CardAction cardAction1;
     [SerializeField] private CardAction cardAction2;
 
-    public string Name => name.WithSpaceBetweenWords();
+    public string Name => name.SkipThroughFirstDash().WithSpaceBetweenWords();
     public ResourceCost Cost => cost;
     public Sprite Art => art;
     public string Description => description;
-    public string TypeDescription => typeDescription;
+    public string TypeDescription => typeDescription.Value;
     public Maybe<string> LimitedToClass => new Maybe<string>(onlyPlayableByClass.Value.Length > 0 ? onlyPlayableByClass.Value : null);
     
     public CardAction[] Actions => Array.Empty<CardAction>()
