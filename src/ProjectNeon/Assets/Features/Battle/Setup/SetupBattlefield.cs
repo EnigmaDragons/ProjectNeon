fe49aa5129b2a2e3f3baff2ee81eefbd2fb8c95b
Silas Reinagel <silas.reinagel@gmail.com>
2020-01-01 15:21:02 -0700

    Fixed Battlefield size. Closes #513 (#549)

--- src/ProjectNeon/Assets/Features/Battle/Setup/SetupBattlefield.cs
+++ src/ProjectNeon/Assets/Features/Battle/Setup/SetupBattlefield.cs
@@ -4,10 +4,12 @@ public class SetupBattlefield : MonoBehaviour
 {
     [SerializeField] private BattleState battleState;
     [SerializeField] private GameEvent onFinished;
+    [SerializeField] private float scale = 0.929f;
 
     private void Awake()
     {
-        Instantiate(battleState.Battlefield, new Vector3(0, 0, 10), Quaternion.identity);
+        var battlefield = Instantiate(battleState.Battlefield, new Vector3(0, 0, 10), Quaternion.identity, transform);
+        battlefield.transform.localScale = new Vector3(scale, scale, scale);
     }
 
     void Start()
