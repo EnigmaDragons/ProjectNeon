7d09f55c7815374629c20a76dd6d0ee1e26b62ca
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-05 19:26:43 -0700

    Added Battle Log. Closes #580

---
+++ src/ProjectNeon/Assets/Core/Variables/BoolVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BoolVariable : ScriptableObject
+{
+    [SerializeField]
+    private bool value = false;
+
+    public bool Value
+    {
+        get { return value; }
+        set { this.value = value; }
+    }
+}
