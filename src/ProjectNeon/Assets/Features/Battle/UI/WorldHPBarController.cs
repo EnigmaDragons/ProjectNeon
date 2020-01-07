325b76b0929bc5f4f26ec9ad8ae7bf7999d595b2
Silas Reinagel <silas.reinagel@carvana.com>
2020-01-06 22:42:17 -0700

    Consolidated HP Bar Code. Closes #582

---
+++ src/ProjectNeon/Assets/Features/Battle/UI/WorldHPBarController.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public sealed class WorldHPBarController : HPBarControllerBase
+{
+    [SerializeField] private GameObject bar;
+    [SerializeField] private TextMeshPro text;
+
+    protected override void SetFillAmount(float amount)
+    {
+        var originalScale = bar.transform.localScale;
+        bar.transform.localScale = new Vector3(amount, originalScale.y, originalScale.z);
+    }
+
+    protected override void SetText(string text) => this.text.text = text;
+}
