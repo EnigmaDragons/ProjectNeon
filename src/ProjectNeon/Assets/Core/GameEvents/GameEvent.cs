bc3f86abb980a153c0beab69a9301fa9bddb8a63
Silas Reinagel <silas.reinagel@gmail.com>
2020-01-13 20:42:34 -0700

    Fixed Card Resolution Race Condition. Closes #610 (#623)

--- src/ProjectNeon/Assets/Core/GameEvents/GameEvent.cs
+++ src/ProjectNeon/Assets/Core/GameEvents/GameEvent.cs
@@ -8,30 +8,13 @@ public class GameEvent : ScriptableObject
 {
     private IEnumerable<GameEventSubscription> listeners = Array.Empty<GameEventSubscription>();
 
-    public void Publish()
-    {
-        //Debug.Log($"Publishing {name} to {listeners.Count()} subscribers");
-        CleansedListeners.ForEach(l =>
-        {
-            //Debug.Log($"Publishing {name} to {l.Owner.GetType()}");
-            l.OnEvent(l);
-        });
-    }
+    public void Publish() => CleansedListeners.ForEach(l => l.OnEvent(l));
 
     public void Subscribe(Action action, object subscriber) => Subscribe(new GameEventSubscription(name, x => action(), subscriber));
     public void Subscribe(GameEventListener listener) => Subscribe(new GameEventSubscription(name, x => listener.OnEventRaised(), listener));
     public void Unsubscribe(GameEventListener listener) => Unsubscribe((object) listener);
-
-    public void Subscribe(GameEventSubscription e)
-    {
-        listeners = CleansedListeners.Concat(e);
-    }
-
-    public void Unsubscribe(object owner)
-    {
-        //Debug.Log($"Unsubscribed {owner.GetType()}");
-        listeners = CleansedListeners.Where(l => !ReferenceEquals(l.Owner, owner));
-    }
+    public void Subscribe(GameEventSubscription e) => listeners = CleansedListeners.Concat(e);
+    public void Unsubscribe(object owner) => listeners = CleansedListeners.Where(l => !ReferenceEquals(l.Owner, owner));
 
     private IEnumerable<GameEventSubscription> CleansedListeners => listeners.Where(x => x.Owner != null);
 }
