734d7e46655c19b8ddca4a3b462d9265ddb4db25
noah reinagel <noah.r9@gmail.com>
2020-07-25 14:46:07 -0700

    fixed resource flat test

--- src/ProjectNeon/Assets/Z_Tests/Features/Effects/ResourceFlat.cs
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/ResourceFlat.cs
@@ -7,15 +7,20 @@ public sealed class ResourceFlat
     private EffectData data = new EffectData { 
         EffectType = EffectType.ResourceFlat, 
         FloatAmount = new FloatReference(5), 
-        EffectScope = new StringReference("Ammo") 
+        EffectScope = new StringReference(Ammo.Name) 
     } ;
 
+    private static IResourceType Ammo = new InMemoryResourceType {Name = "Ammo", MaxAmount = 99, StartingAmount = 0};
+    
     private Member performer = new Member(
         1,
         "Good Dummy One",
         "ElectroBlade",
         TeamType.Party,
         new StatAddends()
+        {
+            ResourceTypes = new IResourceType[] { Ammo }
+        }
     );
 
     [Test]
@@ -24,7 +29,7 @@ public sealed class ResourceFlat
         AllEffects.Apply(data, performer, new Single(performer));
         Assert.AreEqual(
             5,
-            performer.State.ResourceTypes[0].MaxAmount
+            performer.State[Ammo]
         );
     }
 }
