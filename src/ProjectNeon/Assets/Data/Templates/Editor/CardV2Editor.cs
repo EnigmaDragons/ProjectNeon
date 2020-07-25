b55dd1a07160f9ae242921bb8f6ad0008ec3e58d
noah reinagel <noah.r9@gmail.com>
2020-07-24 23:07:34 -0700

    Resource cost now working properly

--- src/ProjectNeon/Assets/Data/Templates/Editor/CardV2Editor.cs
+++ src/ProjectNeon/Assets/Data/Templates/Editor/CardV2Editor.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEditor;
 
-namespace Assets.Data.Templates.Editor
+[CustomEditor(typeof(CardV2))]
+public class CardV2Editor : Editor
 {
-    public class CardV2Editor 
+    private SerializedProperty _art;
+    private SerializedProperty _description;
+    private SerializedProperty _typeDescription;
+    private SerializedProperty _onlyPlayableByClass;
+    private SerializedProperty _cost;
+    private SerializedProperty _classSpecial;
+
+
+    public void OnEnable()
     {
+        _art = serializedObject.FindProperty("art");
+        _description = serializedObject.FindProperty("description");
+        _typeDescription = serializedObject.FindProperty("typeDescription");
+        _onlyPlayableByClass = serializedObject.FindProperty("onlyPlayableByClass");
+        _cost = serializedObject.FindProperty("cost");
+        _classSpecial = serializedObject.FindProperty("classSpecial");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.ObjectField(_art);
+        EditorGUILayout.ObjectField(_description);
+        EditorGUILayout.ObjectField(_typeDescription);
+        EditorGUILayout.ObjectField(_onlyPlayableByClass);
+        EditorGUILayout.ObjectField(_cost);
+        EditorGUILayout.ObjectField(_classSpecial);
     }
 }
---
+++ src/ProjectNeon/Assets/Features/Cards/Editor/ResourceCostDrawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ResourceCost))]
+public class ResourceCostDrawer : PropertyDrawer
+{
+    private SerializedProperty _cost;
+    private SerializedProperty _resourceType;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var cost = _cost ?? (_cost = property.FindPropertyRelative("cost"));
+        var resourceType = _resourceType ?? (_resourceType = property.FindPropertyRelative("resourceType"));
+        Rect contentPosition = EditorGUI.PrefixLabel(position, new GUIContent(property.displayName));
+        float quarter = contentPosition.width / 4;
+        contentPosition.width = quarter;
+        EditorGUI.BeginProperty(contentPosition, label, cost);
+        {
+            EditorGUI.BeginChangeCheck();
+            int newVal = EditorGUI.IntField(contentPosition, new GUIContent(), cost.intValue);
+            if (EditorGUI.EndChangeCheck())
+                cost.intValue = newVal;
+        }
+        EditorGUI.EndProperty();
+        contentPosition.x += quarter;
+        contentPosition.width = quarter * 3;
+        EditorGUI.BeginProperty(contentPosition, label, resourceType);
+        {
+            EditorGUI.BeginChangeCheck();
+            Object newVal = EditorGUI.ObjectField(contentPosition, resourceType.objectReferenceValue, typeof(ResourceType), false);
+            if (EditorGUI.EndChangeCheck())
+                resourceType.objectReferenceValue = newVal;
+        }
+        EditorGUI.EndProperty();
+    }
+}
