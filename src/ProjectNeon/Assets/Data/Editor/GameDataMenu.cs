5191185ef9ddc74821fde068c20c4efc1837cee4
Silas Reinagel <silas.reinagel@carvana.com>
2020-07-27 15:35:09 -0700

    Moved Data Templates. Closes #756

---
+++ src/ProjectNeon/Assets/Data/Editor/GameDataMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+static class GameDataMenu
+{
+    [MenuItem("Assets/Create/Card")]
+    static void Card() => Create<Card>();
+    
+    [MenuItem("Assets/Create/Effect")]
+    static void CreateSequence() => Create<CardActionsData>();
+
+    [MenuItem("Assets/Create/GameContent/Hero")]
+    static void Hero() => Create<Hero>();
+    [MenuItem("Assets/Create/GameContent/Enemy")]
+    static void Enemy() => Create<Enemy>();
+
+    [MenuItem("Assets/Create/GameContent/SimpleResourceType")]
+    static void SimpleResourceType() => Create<SimpleResourceType>();
+    [MenuItem("Assets/Create/GameContent/Deck")]
+    static void Deck() => Create<Deck>();
+    [MenuItem("Assets/Create/GameContent/DumbAI")]
+    static void DumbAI() => Create<DumbAI>();
+
+    private static void Create<T>() where T : ScriptableObject
+    {
+        var asset = ScriptableObject.CreateInstance<T>();
+        var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        path += $"/New{typeof(T).Name}.asset";
+        ProjectWindowUtil.CreateAsset(asset, path);
+    }
+}
