2419f5f3612d30fbcd89c6309148102b538f1ef4
SilasReinagel <silas.reinagel@gmail.com>
2020-07-26 17:39:10 -0700

    Setup Prototype Adventure. Fixed Delayed Payload. Closes #740. Closes #741

--- src/ProjectNeon/Assets/Features/GameProgression/SpecificEncounterSegment.cs
+++ src/ProjectNeon/Assets/Features/GameProgression/SpecificEncounterSegment.cs
@@ -7,8 +7,9 @@ class SpecificEncounterSegment : StageSegment
     [SerializeField] private GameObject battlefield;
     [SerializeField] private Enemy[] enemies;
     [SerializeField] private BattleState battle;
+    [SerializeField] private string displayName = "Boss Battle";
 
-    public override string Name => "Boss Battle";
+    public override string Name => displayName;
     
     public override void Start()
     {
