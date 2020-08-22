d003551a9d986620e748b76c732f3c4b3fd627b3
noah reinagel <noah.r9@gmail.com>
2020-08-21 22:39:57 -0700

    fixed DamageOverTime

---
+++ src/ProjectNeon/Assets/Features/Effects/States/DamageOverTimeState.cs
@@ -0,0 +1,30 @@
+﻿public class DamageOverTimeState : ITemporalState
+{
+    private readonly int _amount;
+    private readonly Member _target;
+    private readonly bool _indefinite;
+    private int _remainingDuration;
+
+    public IStats Stats { get; }
+    public bool IsDebuff => true;
+    public bool IsActive => _remainingDuration > 0 || _indefinite;
+
+    public DamageOverTimeState(int amount, Member target, int turns)
+    {
+        Stats = new StatAddends();
+        _amount = amount;
+        _target = target;
+        _indefinite = turns < 0;
+        _remainingDuration = turns;
+    }
+
+    public void OnTurnStart()
+    {
+        if (!IsActive) return;
+
+        _remainingDuration--;
+        _target.State.TakeRawDamage(_amount);
+    }
+
+    public void OnTurnEnd() {}
+}
