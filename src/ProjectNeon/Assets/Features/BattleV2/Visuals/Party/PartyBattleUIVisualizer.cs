098f323280773fb3a975789a14184fdb222336be
SilasReinagel <silas.reinagel@gmail.com>
2020-08-22 20:36:32 -0700

    Hero Battle UI Summary Area matches order of Heroes. Closes #1008

--- src/ProjectNeon/Assets/Features/BattleV2/Visuals/Party/PartyBattleUIVisualizer.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Visuals/Party/PartyBattleUIVisualizer.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Obsolete]
 public sealed class PartyBattleUIVisualizer : MonoBehaviour
 {
     [SerializeField] private PartyArea partyArea;
