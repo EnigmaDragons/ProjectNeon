bb559f7bb25ff309fa2cd1a60d79cb550ffcc1fb
SilasReinagel <silas.reinagel@gmail.com>
2020-09-02 13:04:56 -0700

    Setup Sub Equipment Pools. Added Electroblade +Starting Energy. Closes #1087. Closes #1054

--- src/ProjectNeon/Assets/Features/Equipment/EquipmentPool.cs
+++ src/ProjectNeon/Assets/Features/Equipment/EquipmentPool.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class EquipmentPool : ScriptableObject
 {
     [SerializeField] private List<StaticEquipment> all;
+    [SerializeField] private EquipmentPool[] subPools = new EquipmentPool[0];
     [SerializeField] private int numRandomCommons = 6;
     [SerializeField] private int numRandomUncommons = 4;
 
     private readonly EquipmentGenerator _generator = new EquipmentGenerator();
     
     public IEnumerable<Equipment> All => all
+        .Concat(subPools.SelectMany(s => s.All))
         .Concat(Enumerable.Range(0, numRandomCommons).Select(_ => _generator.GenerateRandomCommon()))
         .Concat(Enumerable.Range(0, numRandomUncommons).Select(_ => _generator.GenerateRandomUncommon()));
 }
