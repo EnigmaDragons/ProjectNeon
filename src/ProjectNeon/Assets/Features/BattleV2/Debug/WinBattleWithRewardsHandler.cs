8da488e1cb84d136f59cb8b0e7f85a7ce141888c
SilasReinagel <silas.reinagel@gmail.com>
2020-09-20 12:56:05 -0700

    Dev Cheat: Allow Auto Win Battle with full rewards

---
+++ src/ProjectNeon/Assets/Features/BattleV2/Debug/WinBattleWithRewardsHandler.cs
@@ -0,0 +1,17 @@
+
+using System.Linq;
+using UnityEngine;
+
+public class WinBattleWithRewardsHandler : OnMessage<WinBattleWithRewards>
+{
+    [SerializeField] private BattleState state;
+    [SerializeField] private ShopCardPool cardPrizePool;
+    
+    protected override void Execute(WinBattleWithRewards msg)
+    {            
+        var rewardPicker = new ShopSelectionPicker();
+        state.SetRewardCards(rewardPicker.PickCards(state.Party, cardPrizePool, 2));
+        state.AddRewardCredits(state.EnemyArea.Enemies.Sum(e => e.RewardCredits));
+        Message.Publish(new BattleFinished(TeamType.Party));
+    }
+}
