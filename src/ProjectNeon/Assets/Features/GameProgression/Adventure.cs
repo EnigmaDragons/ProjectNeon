0875b7888ef504357b2a533e5a4c47d1202e71b4
SilasReinagel <silas.reinagel@gmail.com>
2020-09-28 20:28:53 -0700

    Content: Added Adventure title and background. Closes #1356

--- src/ProjectNeon/Assets/Features/GameProgression/Adventure.cs
+++ src/ProjectNeon/Assets/Features/GameProgression/Adventure.cs
@@ -1,13 +1,16 @@
 ﻿using System.Linq;
 using UnityEngine;
-
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(menuName = "Adventure/Adventure")]
 public class Adventure : ScriptableObject
 {
     [SerializeField] private Stage[] stages;
+    [SerializeField] private string adventureTitle;
+    [SerializeField, TextArea(4, 10)] private string story;
 
     // @todo #1:15min Design. What happens when the adventure is won?
 
+    public string Story => story;
     public Stage[] Stages => stages.ToArray();
 }
