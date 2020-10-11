9351d7dac474f5647dbafc0d436d1c4c69622b77
SilasReinagel <silas.reinagel@gmail.com>
2020-10-11 01:20:55 -0700

    New Feature: Implemented Blind Effect. Closes #1430

--- src/ProjectNeon/Assets/Features/Combatants/MemberState.cs
+++ src/ProjectNeon/Assets/Features/Combatants/MemberState.cs
@@ -125,7 +125,15 @@ public sealed class MemberState : IStats
         _multiplierMods.Add(mods);
     });
 
-    public void CleanseDebuffs() => RemoveTemporaryEffects(s => s.IsDebuff);
+    public void CleanseDebuffs()
+    {
+        RemoveTemporaryEffects(s => s.IsDebuff);
+        _counters[TemporalStatType.TurnStun.ToString()].Set(0);
+        _counters[TemporalStatType.CardStun.ToString()].Set(0);
+        _counters[TemporalStatType.Confusion.ToString()].Set(0);
+        _counters[TemporalStatType.Blind.ToString()].Set(0);
+    }
+
     public void RemoveTemporaryEffects(Predicate<ITemporalState> condition) => PublishAfter(() =>
     {
         _additiveMods.RemoveAll(condition);
