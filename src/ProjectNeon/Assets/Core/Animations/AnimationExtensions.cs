b716d5aba15656992361669f4222732976d08f75
noah reinagel <noah.r9@gmail.com>
2020-10-11 00:17:31 -0700

    now exits if missing animation

--- src/ProjectNeon/Assets/Core/Animations/AnimationExtensions.cs
+++ src/ProjectNeon/Assets/Core/Animations/AnimationExtensions.cs
@@ -1,27 +1,36 @@
 
 using System;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public static class AnimationExtensions
 {
     public static IEnumerator PlayAnimationUntilFinished(this Animator animator, string animationName, Action<float> onFinished, int layer = 0, float maxTime = 5f)
     {
-        var elapsed = 0f; 
-        animator.SetTrigger(animationName);
-        bool hasStartedAnimated = false;
-        while (elapsed < maxTime)
+        if (animator.parameters.Any(x => x.name == animationName))
         {
-            yield return new WaitForSeconds(0.1f);
-            elapsed += 0.1f;
-            var isPlayingAnimation = animator.GetCurrentAnimatorStateInfo(layer).IsName(animationName);
-            if (isPlayingAnimation && !hasStartedAnimated)
-                hasStartedAnimated = true;
-            if (!isPlayingAnimation && hasStartedAnimated)
-                break;
+            var elapsed = 0f; 
+            animator.SetTrigger(animationName);
+            bool hasStartedAnimated = false;
+            while (elapsed < maxTime)
+            {
+                yield return new WaitForSeconds(0.1f);
+                elapsed += 0.1f;
+                var isPlayingAnimation = animator.GetCurrentAnimatorStateInfo(layer).IsName(animationName);
+                if (isPlayingAnimation && !hasStartedAnimated)
+                    hasStartedAnimated = true;
+                if (!isPlayingAnimation && hasStartedAnimated)
+                    break;
+            }
+            if (!hasStartedAnimated)
+                Log.Error($"Animation: {animationName} never started");
+            onFinished(elapsed);
         }
-        if (!hasStartedAnimated)
-            Log.Error($"Animation: {animationName} never started");
-        onFinished(elapsed);
+        else
+        {
+            Log.Error($"Animation: {animationName} not found");
+            onFinished(0);
+        }
     }
 }
