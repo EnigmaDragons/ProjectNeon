4563225230dc767e5df979b42db4df1278fbd601
SilasReinagel <silas.reinagel@gmail.com>
2020-10-11 15:33:51 -0700

    UI: Shows Evaded and Missed flying texts. Closes #1453

--- src/ProjectNeon/Assets/Features/BattleV2/Visuals/Messages/PlayRawBattleEffect.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Visuals/Messages/PlayRawBattleEffect.cs
@@ -4,11 +4,12 @@ public sealed class PlayRawBattleEffect
 {
     public string EffectName { get; }
     public Vector3 Target { get; }
+    public string Detail { get; }
 
-    public PlayRawBattleEffect(string effectName, Vector3 target)
+    public PlayRawBattleEffect(string effectName, Vector3 target, string detail = "")
     {
         EffectName = effectName;
         Target = target;
+        Detail = detail;
     }
-    
 }
