bca27970c1a8bb7d13c2bb904a18a624166c08a1
MichaelElkan <melkan22@gmail.com>
2020-11-08 04:56:06 +0000

    Bug: Fixed Group: All, Scope: One in card targeting. Closes #1549

--- src/ProjectNeon/Assets/Features/BattleV2/Targeting/BattleStateTargetingExtensions.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Targeting/BattleStateTargetingExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using UnityEngine;
 
 public static class BattleStateTargetingExtensions
 {
@@ -18,6 +19,9 @@ public static class BattleStateTargetingExtensions
     
     public static Member[] GetConscious(this Member[] members, TeamType team) 
         => members.Where(x => x.TeamType == team && x.IsConscious()).ToArray();
+
+    public static Member[] GetConscious(this Member[] members)
+        => members.Where(x => x.IsConscious()).ToArray();
     
     public static Member[] GetConsciousAllies(this BattleState s, Member m)
         => s.MembersWithoutIds.GetConscious(m.TeamType);
@@ -37,10 +41,7 @@ public static class BattleStateTargetingExtensions
                 targets = new Target[] { new MemberAsTarget(self) };
                 break;
             }
-            case Group.All:  {
-                targets = Targets(new Team(allMembers.Where(m => m.IsConscious())));
-                break;
-            }
+            case Group.All:  
             case Group.Opponent:
             case Group.Ally:
             {
@@ -48,19 +49,22 @@ public static class BattleStateTargetingExtensions
                 break;
             }
         }
+        
         return targets;
     }
 
     private static Target[] NonSelfConsciousTargetsFor(this Member[] allMembers, Member originator, TeamType myTeam, Group group, Scope scope)
     {
         var opponentsAre = myTeam == TeamType.Party ? TeamType.Enemies : TeamType.Party;
-        var teamMembers = group == Group.Ally ? GetConscious(allMembers, myTeam) : GetConscious(allMembers, opponentsAre);
+        var teamMembers = group == Group.Ally ? GetConscious(allMembers, myTeam) : group == Group.Opponent ? GetConscious(allMembers, opponentsAre) : GetConscious(allMembers) ;
+        
 
         if (scope == Scope.One)
         {
+            var doesEnemyHaveTaunt = teamMembers.Any(m => m.HasTaunt() && m.TeamType != myTeam);
             var membersWithTaunt = teamMembers.Where(m => m.HasTaunt() || m.TeamType == myTeam).ToArray();
             var membersWithoutStealth = teamMembers.Where(m => !m.IsStealth() || m.TeamType == myTeam).ToArray();
-            var members = membersWithTaunt.Any() 
+            var members = doesEnemyHaveTaunt 
                 ? membersWithTaunt 
                 : membersWithoutStealth.Any() 
                     ? membersWithoutStealth 
