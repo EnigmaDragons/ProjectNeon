48e9fc39c7b2d7f9caa556be0bebebd7cffb7d12
noah reinagel <noah.r9@gmail.com>
2020-11-22 16:11:47 -0700

    Now pipes through before starting combat

--- src/ProjectNeon/Assets/Features/Map/EnableOnMapVersion.cs
+++ src/ProjectNeon/Assets/Features/Map/EnableOnMapVersion.cs
@@ -6,9 +6,5 @@ public class EnableOnMapVersion : MonoBehaviour
     [SerializeField] private bool version1Only;
     [SerializeField] private GameObject toEnable;
 
-    private void Awake()
-    {
-        if ((current.Adventure.IsV2 && !version1Only) || (!current.Adventure.IsV2 && version1Only))
-            toEnable.SetActive(true);
-    }
+    private void OnEnable() => toEnable.SetActive((current.Adventure.IsV2 && !version1Only) || (!current.Adventure.IsV2 && version1Only));
 }
