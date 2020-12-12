9b79eeb176835b1d81c36f040fbab025ec5bf593
SilasReinagel <silas.reinagel@gmail.com>
2020-12-12 14:40:20 -0700

    Coding: Centralized Map UI Configuration. Closes #1658

--- src/ProjectNeon/Assets/Features/Map/GameMap2.cs
+++ src/ProjectNeon/Assets/Features/Map/GameMap2.cs
@@ -3,19 +3,11 @@
 [CreateAssetMenu]
 public class GameMap2 : ScriptableObject
 {
-    [SerializeField] private GameObject artPrototype;
+    [SerializeField] private MovableMap artPrototype;
     [SerializeField][Range(0,99)] private int minPaths;
     [SerializeField][Range(0,99)] private int maxPaths;
-    [SerializeField] private float bottomMargin;
-    [SerializeField] private float leftMargin;
-    [SerializeField] private float rightMargin;
-    [SerializeField] private float topMargin = 254;
     
-    public GameObject ArtPrototype => artPrototype;
+    public MovableMap ArtPrototype => artPrototype;
     public int MinPaths => minPaths;
     public int MaxPaths => maxPaths;
-    public float BottomMargin => bottomMargin;
-    public float LeftMargin => leftMargin;
-    public float RightMargin => rightMargin;
-    public float TopMargin => topMargin;
 }
