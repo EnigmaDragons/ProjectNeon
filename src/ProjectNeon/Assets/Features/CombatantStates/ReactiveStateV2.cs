382f1443cba8dc3eda68903ab50cc66ca2a86be2
noah reinagel <noah.r9@gmail.com>
2020-12-11 22:28:55 -0700

    Added a way for card action avoided to be able to trigger their effects closes #1638

--- src/ProjectNeon/Assets/Features/CombatantStates/ReactiveStateV2.cs
+++ src/ProjectNeon/Assets/Features/CombatantStates/ReactiveStateV2.cs
@@ -1,4 +1,5 @@
 public interface ReactiveStateV2 : ITemporalState
 {
     Maybe<ProposedReaction> OnEffectResolved(EffectResolved e);
+    Maybe<ProposedReaction> OnCardActionAvoided(CardActionAvoided e);
 }
