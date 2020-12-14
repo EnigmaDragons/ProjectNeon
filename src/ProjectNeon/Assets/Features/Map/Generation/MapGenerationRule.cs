f43f22b7ca2e50767db741120183f0516bfc8a67
noah reinagel <noah.r9@gmail.com>
2020-12-13 19:05:39 -0700

    no non combat in a row closes #1654

--- src/ProjectNeon/Assets/Features/Map/Generation/MapGenerationRule.cs
+++ src/ProjectNeon/Assets/Features/Map/Generation/MapGenerationRule.cs
@@ -3,5 +3,5 @@ using System.Collections.Generic;
 
 public abstract  class MapGenerationRule
 {
-    public abstract List<MapNodeType> FilterNodeTypes(List<MapNodeType> remainingTypes, List<List<MapNode>> map, MapNode node, int column);
+    public abstract List<MapNodeType> FilterNodeTypes(List<MapNodeType> remainingTypes, List<List<MapNode>> map, MapNode node, int column, List<MapNode> toNodes);
 }
---
+++ src/ProjectNeon/Assets/Features/Map/Generation/Rules/NoNonCombatDuplicates.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class NoNonCombatDuplicates : MapGenerationRule
+{
+    public override List<MapNodeType> FilterNodeTypes(List<MapNodeType> remainingTypes, List<List<MapNode>> map, MapNode node, int column, List<MapNode> toNodes)
+        => remainingTypes.Where(type => type == MapNodeType.Combat || type == MapNodeType.Elite || toNodes.All(to => to.Type != type)).ToList();
+}
