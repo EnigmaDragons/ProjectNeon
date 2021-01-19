e85f6a88797f638528848f652ee619c98e55b3ad
Silas Reinagel <silas.reinagel@carvana.com>
2021-01-19 13:41:45 -0700

    UI: Fixed card jumping when cursor at bottom of screen on hand. Closes #1867

--- src/ProjectNeon/Assets/Core/Strings/StringKeyValuePair.cs
+++ src/ProjectNeon/Assets/Core/Strings/StringKeyValuePair.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [Serializable]
 public class StringKeyValuePair
 {
+    public string Name => Key.Value;
     public StringReference Key;
     [SerializeField, TextArea(1, 12)]public string Value;
 }
