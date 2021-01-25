51a479bcd1d2ff9ced7d2763f548d5a0c523b579
SilasReinagel <silas.reinagel@gmail.com>
2021-01-24 19:18:37 -0700

    UI: Wired up status icon for Aikido Hyper Reflexers. React On Evaded with Card. Closes #1962.

--- src/ProjectNeon/Assets/Features/Effects/AllEffects.cs
+++ src/ProjectNeon/Assets/Features/Effects/AllEffects.cs
@@ -34,7 +34,7 @@ public static class AllEffects
         { EffectType.InterceptAttackForTurns, e => new InterceptAttack(e.NumberOfTurns)},
         { EffectType.Attack, e => new Attack(new PhysicalDamage(e.BaseAmount, e.FloatAmount), e.HitsRandomTargetMember)},
         { EffectType.HealOverTime, e => new HealOverTime(e.FloatAmount, e.NumberOfTurns) },
-        { EffectType.ReactOnEvadedWithCard, e => new EffectOnEvaded(false, e.IntAmount, e.NumberOfTurns, ReactiveTriggerScopeExtensions.Parse(e.EffectScope),e.ReactionSequence) },
+        { EffectType.ReactOnEvadedWithCard, e => new EffectOnEvaded(false, e.IntAmount, e.NumberOfTurns, e.StatusDetail, ReactiveTriggerScopeExtensions.Parse(e.EffectScope),e.ReactionSequence) },
         { EffectType.HealMagic, e => new Heal(e.BaseAmount, e.FloatAmount, StatType.Magic) },
         { EffectType.HealToughness, e => new Heal(e.BaseAmount, e.FloatAmount, StatType.Toughness) },
         { EffectType.AdjustPrimaryResource, e => new SimpleEffect(m => BattleLogged($"{m.Name} {GainedOrLostTerm(e.TotalIntAmount)} {Math.Abs(e.TotalIntAmount)} {m.PrimaryResource.Name}", 
--- src/ProjectNeon/Assets/Features/Effects/Editor/EffectDataEditor.cs
+++ src/ProjectNeon/Assets/Features/Effects/Editor/EffectDataEditor.cs
@@ -13,7 +13,7 @@ public class EffectDataEditor : PropertyDrawer
     private DictionaryWithDefault<EffectType, string[]> _relevantProperties = new DictionaryWithDefault<EffectType, string[]>(new string[] { "BaseAmount", "FloatAmount", "NumberOfTurns", "HitsRandomTargetMember" })
     {
         {EffectType.Nothing, new string[0]},
-        {EffectType.ReactOnEvadedWithCard, new []{ "FloatAmount", "NumberOfTurns", "EffectScope", "ReactionSequence" }},
+        {EffectType.ReactOnEvadedWithCard, new []{ "FloatAmount", "NumberOfTurns", "EffectScope", "StatusTag", "StatusDetailText", "ReactionSequence" }},
         {EffectType.ReactWithEffect, new []{ "FloatAmount", "NumberOfTurns", "EffectScope", "StatusTag", "StatusDetailText", "ReactionConditionType", "ReactionEffect"}},
         {EffectType.ReactWithCard, new []{ "FloatAmount", "NumberOfTurns", "EffectScope", "StatusTag", "StatusDetailText", "ReactionConditionType", "ReactionSequence"}},
         {EffectType.ResourceFlat, new [] { "FloatAmount", "NumberOfTurns", "EffectScope" }},
