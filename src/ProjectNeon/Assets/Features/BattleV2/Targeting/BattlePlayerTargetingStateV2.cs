c5505a4ce890004d2f9e3f50aade6525bfe7bff3
noah reinagel <noah.r9@gmail.com>
2021-02-13 21:25:45 -0700

    couldn't solve while target prefab not showing

--- src/ProjectNeon/Assets/Features/BattleV2/Targeting/BattlePlayerTargetingStateV2.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Targeting/BattlePlayerTargetingStateV2.cs
@@ -15,39 +15,21 @@ public class BattlePlayerTargetingStateV2 : ScriptableObject
     public bool HasValidTargets => !MemberToTargetMap.Any() || TargetMember.IsPresent;
     public Target[] Targets => GetTargets.Select(x => x()).ToArray();
 
-    public void InitCardForSelection(Card card)
+    public void IndicateMember(int memberId)
     {
-        GetTargets = new List<Func<Target>>();
-        MemberToTargetMap = new List<Dictionary<int, Target>>();
-        TargetMember = Maybe<int>.Missing();
-        var targetMaps = 0;
-        foreach (var sequence in card.ActionSequences)
+        if ((TargetMember.IsPresent && TargetMember.Value == memberId) || !MemberToTargetMap.Any())
+            return;
+        if (MemberToTargetMap.All(x => x.ContainsKey(memberId)))
         {
-            if (sequence.Group == Group.All || sequence.Scope == Scope.All || sequence.Scope == Scope.AllExceptSelf)
-            {
-                var target = battleState.GetPossibleConsciousTargets(card.Owner, sequence.Group, sequence.Scope).First();
-                GetTargets.Add(() => target);
-            }
-            else
-            {
-                var tmp = targetMaps;
-                GetTargets.Add(() => MemberToTargetMap[tmp][TargetMember.Value]);
-                var scopeOne = battleState.GetPossibleConsciousTargets(card.Owner, sequence.Group, Scope.One);
-                MemberToTargetMap.Add(battleState.GetPossibleConsciousTargets(card.Owner, sequence.Group, sequence.Scope)
-                    .ToDictionary(x =>
-                    {
-                        if (sequence.Scope == Scope.One || sequence.Scope == Scope.OneExceptSelf)
-                            return x.Members[0].Id;
-                        if (sequence.Scope == Scope.AllExcept)
-                            return scopeOne.First(targeted => x.Members.All(member => member.Id != targeted.Members[0].Id)).Members[0].Id;
-                        throw new Exception($"Scope {sequence.Scope} not supported for card {sequence.Group}");
-                    }, x => x));
-                targetMaps++;
-            }
+            TargetMember = memberId;
+            Message.Publish(new TargetChanged(MemberToTargetMap[0][memberId]));
+        }
+        else if (!TargetMember.IsMissing)
+        {
+            TargetMember = Maybe<int>.Missing();
+            Message.Publish(new TargetChanged());
         }
     }
-
-    public void IndicateMember(int memberId) => TargetMember = MemberToTargetMap.All(x => x.ContainsKey(memberId)) ? memberId : Maybe<int>.Missing();
 
     public void StopIndicating() => TargetMember = Maybe<int>.Missing();
 }
