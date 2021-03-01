0075de9f242a42f7271fefdaa6923e751162d26c
noah reinagel <noah.r9@gmail.com>
2021-02-28 18:36:45 -0700

    fixed bug with minions being unspawnable closes #2256

--- src/ProjectNeon/Assets/Features/BattleV2/Enemies/MinionTeamRule.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Enemies/MinionTeamRule.cs
@@ -13,13 +13,14 @@ public class MinionTeamRule : EncounterBuildingRule
 
     public EncounterBuildingContext Filter(EncounterBuildingContext ctx)
     {
-        if (ctx.PossibleEnemies.All(x => x.Tier == EnemyTier.Minion) || ctx.PossibleEnemies.All(x => x.Tier != EnemyTier.Minion))
-            return ctx;
+        if (ctx.PossibleEnemies.All(x => x.Tier == EnemyTier.Minion) 
+            || ctx.PossibleEnemies.All(x => x.Tier != EnemyTier.Minion) 
+            || ctx.SelectedEnemies.Any(x => x.Tier != EnemyTier.Minion))
+            return ctx; 
         return (ctx.SelectedEnemies.Length == 0 
                     && Rng.Chance(_minionTeamChance) 
                     && ctx.PossibleEnemies.Where(x => x.Tier == EnemyTier.Minion).Average(x => x.PowerLevel) * _maxEnemies >= ctx.TargetDifficulty)
-               || (ctx.SelectedEnemies.Length > 0 
-                   && ctx.SelectedEnemies.All(x => x.Tier == EnemyTier.Minion))
+                || ctx.SelectedEnemies.Length > 1
             ? ctx.WithPossibilities(x => x.Tier == EnemyTier.Minion)
             : ctx.WithPossibilities(x => x.Tier != EnemyTier.Minion);
     }
