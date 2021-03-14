cdfa3feb154e79b772ec29d2739f9889252ed4cc
SilasReinagel <silas.reinagel@gmail.com>
2021-03-14 12:15:10 -0700

    UI: Replaced Map Legend with mouse hover. Closes #2397

--- src/ProjectNeon/Assets/Features/Map/MapNodeGameObject.cs
+++ src/ProjectNeon/Assets/Features/Map/MapNodeGameObject.cs
@@ -6,8 +6,10 @@ public class MapNodeGameObject : MonoBehaviour, IPointerEnterHandler, IPointerEx
 {
     [SerializeField] private Button button;
     [SerializeField] private StageSegment segment;
+    [SerializeField] private GameObject hoverRulesPanel;
 
     private IStageSegment _arrivalSegment;
+    private Maybe<GameObject> _rulesPanel;
     
     public void Init(string nodeId, bool canTravelTo)
     {
@@ -16,8 +18,23 @@ public class MapNodeGameObject : MonoBehaviour, IPointerEnterHandler, IPointerEx
         button.onClick.AddListener(() => Message.Publish(new TravelToNode { Node = gameObject, OnArrive = () => _arrivalSegment.Start(), NodeId = nodeId }));
     }
 
+    private void Awake()
+    {
+        _rulesPanel = hoverRulesPanel != null ? new Maybe<GameObject>(hoverRulesPanel) : Maybe<GameObject>.Missing();
+        _rulesPanel.IfPresent(r => r.SetActive(false));
+    }
+
     public void ConvertToDeterministic(AdventureGenerationContext ctx) => _arrivalSegment = segment.GenerateDeterministic(ctx);
     public void SetCanTravelTo(bool canTravelTo) => button.interactable = canTravelTo;
-    public void OnPointerEnter(PointerEventData eventData) => _arrivalSegment.Detail.IfPresent(detail => Message.Publish(new ShowTooltip(detail, true)));
-    public void OnPointerExit(PointerEventData eventData) => Message.Publish(new HideTooltip());
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _arrivalSegment.Detail.IfPresent(detail => Message.Publish(new ShowTooltip(detail, true)));
+        _rulesPanel.IfPresent(r => r.SetActive(true));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _rulesPanel.IfPresent(r => r.SetActive(false));
+        Message.Publish(new HideTooltip());
+    }
 }
