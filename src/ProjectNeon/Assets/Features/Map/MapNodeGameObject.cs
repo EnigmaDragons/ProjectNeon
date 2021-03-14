ccebf656211c5e3f163b7dcdc7f3c7a57cb78a78
SilasReinagel <silas.reinagel@gmail.com>
2021-03-14 01:07:23 -0700

    UI: Shows Enemy List on Combat node when hovering on Map. Closes #2379

--- src/ProjectNeon/Assets/Features/Map/MapNodeGameObject.cs
+++ src/ProjectNeon/Assets/Features/Map/MapNodeGameObject.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class MapNodeGameObject : MonoBehaviour
+public class MapNodeGameObject : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Button button;
     [SerializeField] private StageSegment segment;
@@ -15,10 +16,8 @@ public class MapNodeGameObject : MonoBehaviour
         button.onClick.AddListener(() => Message.Publish(new TravelToNode { Node = gameObject, OnArrive = () => _arrivalSegment.Start(), NodeId = nodeId }));
     }
 
-    public void ConvertToDeterministic(AdventureGenerationContext ctx)
-    {
-        _arrivalSegment = segment.GenerateDeterministic(ctx);
-    }
-    
+    public void ConvertToDeterministic(AdventureGenerationContext ctx) => _arrivalSegment = segment.GenerateDeterministic(ctx);
     public void SetCanTravelTo(bool canTravelTo) => button.interactable = canTravelTo;
+    public void OnPointerEnter(PointerEventData eventData) => _arrivalSegment.Detail.IfPresent(detail => Message.Publish(new ShowTooltip(detail, true)));
+    public void OnPointerExit(PointerEventData eventData) => Message.Publish(new HideTooltip());
 }
