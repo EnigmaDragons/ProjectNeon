03cbe60afa657e41764700859998cc4b52c20542
SilasReinagel <silas.reinagel@gmail.com>
2021-03-14 13:22:03 -0700

    Bug Fix: Added protection against Card Rules Presenter run ender. Closes #2403

--- src/ProjectNeon/Assets/Features/Cards/UI/CardRulesPresenter.cs
+++ src/ProjectNeon/Assets/Features/Cards/UI/CardRulesPresenter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -17,34 +18,47 @@ public class CardRulesPresenter : MonoBehaviour
 
     public void Show(CardTypeData d)
     {
-        Hide();
-        var rulesToShow = new List<string>();
-        rulesToShow.AddIf("X-Cost", d.Cost.PlusXCost);
-        rulesToShow.AddIf("Chain", d.ChainedCard.IsPresent);
-        
-        var battleEffects = d.BattleEffects().Concat(d.ReactionBattleEffects());
-        battleEffects.ForEach(b =>
+        if (d == null)
         {
-            rulesToShow.AddIf("Vulnerable", b.EffectType == EffectType.ApplyVulnerable);
-            rulesToShow.AddIf(TemporalStatType.Disabled.ToString(), b.EffectType == EffectType.DisableForTurns);
-            rulesToShow.AddIf("Stealth", b.EffectType == EffectType.EnterStealth);
-            
-            AddAllMatchingEffectScopeRules(rulesToShow, b, 
-                TemporalStatType.Evade.ToString(), 
-                TemporalStatType.Taunt.ToString(), 
-                TemporalStatType.Blind.ToString(),
-                TemporalStatType.Inhibit.ToString(),
-                TemporalStatType.Spellshield.ToString(),
-                TemporalStatType.CardStun.ToString(),
-                TemporalStatType.DoubleDamage.ToString(),
-                PlayerStatType.CardCycles.ToString(),
-                TemporalStatType.Disabled.ToString(),
-                TemporalStatType.Stealth.ToString());
-        });
+            Log.Error($"Rules - Card Type Data is Null");
+            return;
+        }
         
-        rulesToShow
-            .Distinct()
-            .ForEach(r => Instantiate(rulePresenterPrototype, rulesParent.transform).Initialized(r));
+        try
+        {
+            Hide();
+            var rulesToShow = new List<string>();
+            rulesToShow.AddIf("X-Cost", d.Cost.PlusXCost);
+            rulesToShow.AddIf("Chain", d.ChainedCard.IsPresent);
+
+            var battleEffects = d.BattleEffects().Concat(d.ReactionBattleEffects());
+            battleEffects.ForEach(b =>
+            {
+                rulesToShow.AddIf("Vulnerable", b.EffectType == EffectType.ApplyVulnerable);
+                rulesToShow.AddIf(TemporalStatType.Disabled.ToString(), b.EffectType == EffectType.DisableForTurns);
+                rulesToShow.AddIf("Stealth", b.EffectType == EffectType.EnterStealth);
+
+                AddAllMatchingEffectScopeRules(rulesToShow, b,
+                    TemporalStatType.Evade.ToString(),
+                    TemporalStatType.Taunt.ToString(),
+                    TemporalStatType.Blind.ToString(),
+                    TemporalStatType.Inhibit.ToString(),
+                    TemporalStatType.Spellshield.ToString(),
+                    TemporalStatType.CardStun.ToString(),
+                    TemporalStatType.DoubleDamage.ToString(),
+                    PlayerStatType.CardCycles.ToString(),
+                    TemporalStatType.Disabled.ToString(),
+                    TemporalStatType.Stealth.ToString());
+            });
+
+            rulesToShow
+                .Distinct()
+                .ForEach(r => Instantiate(rulePresenterPrototype, rulesParent.transform).Initialized(r));
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Card Rules Error for {d.Name}", e);
+        }
     }
 
     private void AddAllMatchingEffectScopeRules(List<string> rulesToShow, EffectData e, params string[] scopes) 
