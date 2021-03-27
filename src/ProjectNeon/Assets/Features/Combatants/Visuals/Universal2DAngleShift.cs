4f5aa9965edf814541da10eb3bf93069f4a4be2d
noah reinagel <noah.r9@gmail.com>
2021-03-27 16:22:10 -0700

    universalized camera rotation

---
+++ src/ProjectNeon/Assets/Features/Combatants/Visuals/Universal2DAngleShift.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Universal2DAngleShift : MonoBehaviour
+{
+    private const float angle = 20;
+
+    private void Awake()
+    {
+        transform.rotation = Quaternion.Euler(angle, 0, 0);
+    }
+}
