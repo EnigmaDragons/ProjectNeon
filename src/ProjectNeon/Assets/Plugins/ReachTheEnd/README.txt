b23bccda1dd4e861aa962631616e04aabfabd8f8
noah reinagel <noah.r9@gmail.com>
2021-03-28 00:55:54 -0700

    Tried to make a nightclub

---
+++ src/ProjectNeon/Assets/Plugins/ReachTheEnd/README.txt
@@ -0,0 +1,8 @@
+Sci-Fi Neon Props Pack
+	-AAA Quality
+	-For Console & PC & VR
+	-14 Prefabs
+	-2048x2048 Textures
+	-Low poly with Normal Map
+	-Using Emission
+Enjoy!
---
+++ src/ProjectNeon/Assets/UI/Scripts/SetAmbientLightOnStart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SetAmbientLightOnStart : MonoBehaviour
+{
+    [SerializeField] private Color color;
+    [SerializeField] private float intensity;
+    
+    private void Start()
+    {
+        RenderSettings.ambientLight = color;
+        RenderSettings.ambientIntensity = intensity;
+        DynamicGI.UpdateEnvironment();
+    }   
+}
