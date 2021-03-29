bf2a50330abc062213686a7615ad1a6f4226911c
SilasReinagel <silas.reinagel@gmail.com>
2021-03-28 18:23:44 -0700

    Battle Log: Expresses when damage was impacted by target being vulnerable. Closes #2537

--- src/ProjectNeon/Assets/Features/Effects/DealDamage.cs
+++ src/ProjectNeon/Assets/Features/Effects/DealDamage.cs
@@ -14,10 +14,11 @@ public sealed class DealDamage : Effect
         ctx.Target.Members.ForEach(m =>
         {
             var amount = Mathf.CeilToInt(_damage.Calculate(ctx, m) * m.State.Damagability());
+            var wasVulnerableString = m.IsVulnerable() ? " (Vulnerable) " : " ";
             if (m.State.Damagability() < 0.01)
-                BattleLog.Write($"{m.Name} is Invincible");
+                BattleLog.Write($"0 damage was dealt to Invincible {m.Name}");
             else
-                BattleLog.Write($"{amount} damage dealt to {m.Name}");
+                BattleLog.Write($"{amount} damage dealt to{wasVulnerableString}{m.Name}");
             m.State.TakeDamage(amount);
         });
     }
