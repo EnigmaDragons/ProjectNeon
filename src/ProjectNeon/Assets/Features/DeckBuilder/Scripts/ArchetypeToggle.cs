e1d56c45f0b27934dbf1f4d1b93752082a561ec4
noah reinagel <noah.r9@gmail.com>
2021-05-22 15:16:58 -0700

    UI: used the colors as filters improved the deck ui to not feel as squshed and fixed the hover ability of the deck closes #2879 closes #2876

--- src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/ArchetypeToggle.cs
+++ src/ProjectNeon/Assets/Features/DeckBuilder/Scripts/ArchetypeToggle.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class ArchetypeToggle : MonoBehaviour
 {
     [SerializeField] private Toggle toggle;
-    [SerializeField] private Image image;
-    [SerializeField] private ArchetypeIcons icons;
+    [SerializeField] private TintToggle tintToggle;
+    [SerializeField] private ArchetypeTints tints;
 
     public void Init(string archetype, Action<bool> action)
     {
-        image.sprite = icons.ForArchetypes(new HashSet<string> {archetype});
+        var color = tints.ForArchetypes(new HashSet<string> {archetype});
+        tintToggle.on = new Color(color.r, color.g, color.b, tintToggle.on.a);;
+        tintToggle.off = new Color(color.r, color.g, color.b, tintToggle.off.a);
+        tintToggle.UpdateTint(false);
         toggle.onValueChanged.AddListener(x => action(x));
     }
 }
--- src/ProjectNeon/Assets/UI/Scripts/TintToggle.cs
+++ src/ProjectNeon/Assets/UI/Scripts/TintToggle.cs
@@ -7,8 +7,8 @@ public class TintToggle : MonoBehaviour
     [SerializeField] private Toggle toggle;
     [SerializeField] private Image[] images;
     [SerializeField] private TextMeshProUGUI[] texts;
-    [SerializeField] private Color on;
-    [SerializeField] private Color off;
+    [SerializeField] public Color on;
+    [SerializeField] public Color off;
 
     private void Awake()
     {
@@ -18,7 +18,7 @@ public class TintToggle : MonoBehaviour
 
     private void OnEnable() => UpdateTint(toggle.isOn);
 
-    private void UpdateTint(bool isOn)
+    public void UpdateTint(bool isOn)
     {
         images.ForEach(image => image.color = isOn ? on : off);
         texts.ForEach(text => text.color = isOn ? on : off);
