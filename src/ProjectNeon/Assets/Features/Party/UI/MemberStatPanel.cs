95594ea3f1aeee878a899d46deb71d6c134f68b5
noah reinagel <noah.r9@gmail.com>
2021-06-04 22:24:37 -0700

    *Now member stat panels hide 0s and are organized to be more intuitive

--- src/ProjectNeon/Assets/Features/Party/UI/MemberStatPanel.cs
+++ src/ProjectNeon/Assets/Features/Party/UI/MemberStatPanel.cs
@@ -27,18 +27,23 @@ public sealed class MemberStatPanel : MonoBehaviour
         maxShieldLabel.color = ColorFor(difference.MaxShield());
         attackLabel.text = $"{m.Attack()}";
         attackLabel.color = ColorFor(difference.Attack());
+        attackLabel.gameObject.transform.parent.gameObject.SetActive(m.Attack() > 0);
         magicLabel.text = $"{m.Magic()}";
         magicLabel.color = ColorFor(difference.Magic());
+        magicLabel.gameObject.transform.parent.gameObject.SetActive(m.Magic() > 0);
         leaderLabel.text = $"{m.Leadership()}";
         leaderLabel.color = ColorFor(difference.Leadership());
+        leaderLabel.gameObject.transform.parent.gameObject.SetActive(m.Leadership() > 0);
         armorLabel.text = $"{m.Armor()}";
         armorLabel.color = ColorFor(difference.Armor());
         resistLabel.text = $"{m.Resistance()}";
         resistLabel.color = ColorFor(difference.Resistance());
         toughLabel.text = $"{m.Toughness()}";
         toughLabel.color = ColorFor(difference.Toughness());
+        toughLabel.gameObject.transform.parent.gameObject.SetActive(m.Toughness() > 0);
         econLabel.text = $"{m.Economy()}";
         econLabel.color = ColorFor(difference.Economy());
+        econLabel.gameObject.transform.parent.gameObject.SetActive(m.Economy() > 0);
         return this;
     }
 
@@ -48,12 +53,17 @@ public sealed class MemberStatPanel : MonoBehaviour
         startingShieldLabel.text = $"{s.StartingShield()}";
         maxShieldLabel.text = $"{s.MaxShield()}";
         attackLabel.text = $"{s.Attack()}";
+        attackLabel.gameObject.transform.parent.gameObject.SetActive(s.Attack() > 0);
         magicLabel.text = $"{s.Magic()}";
+        magicLabel.gameObject.transform.parent.gameObject.SetActive(s.Magic() > 0);
         leaderLabel.text = $"{s.Leadership()}";
+        leaderLabel.gameObject.transform.parent.gameObject.SetActive(s.Leadership() > 0);
         armorLabel.text = $"{s.Armor()}";
         resistLabel.text = $"{s.Resistance()}";
         toughLabel.text = $"{s.Toughness()}";
+        toughLabel.gameObject.transform.parent.gameObject.SetActive(s.Toughness() > 0);
         econLabel.text = $"{s.Economy()}";
+        econLabel.gameObject.transform.parent.gameObject.SetActive(s.Economy() > 0);
         return this;
     }
 
