8aa57529db1829d6d726256aab897e7d4876e97c
noah reinagel <noah.r9@gmail.com>
2021-06-06 14:21:41 -0700

    Level Up Tree: Electroblade's levelup tree has been spruced up

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/TargetIsDefenseless.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/TargetIsDefenceless")]
+public class TargetIsDefenseless : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+        => ctx.Target.Members.Any(x => x.Armor() > 0 || x.Resistance() > 0)
+            ? $"Target has a member with Armor or Resistance"
+            : Maybe<string>.Missing();
+}
