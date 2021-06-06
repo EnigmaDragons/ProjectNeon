dd246861b2f4bbea5282db1bf30388c542e705d7
noah reinagel <noah.r9@gmail.com>
2021-06-06 16:36:23 -0700

    Level Up Tree: enhanced soldier's level up tree

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/NoGrenadesCondition.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/No Grenades")]
+public class NoGrenadesCondition : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.Source.State.ResourceAmount("Grenades") > 0
+            ? "Have grenades"
+            : Maybe<string>.Missing();
+    }
+}
