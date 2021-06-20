ab22703fa643fba66a85a9dc0116c792a5f742c9
noah reinagel <noah.r9@gmail.com>
2021-06-20 16:00:12 -0700

    *fixed

--- src/ProjectNeon/Assets/Features/Map/CurrentGameMap3.cs
+++ src/ProjectNeon/Assets/Features/Map/CurrentGameMap3.cs
@@ -10,7 +10,7 @@ public class CurrentGameMap3 : ScriptableObject
     public Vector2 CurrentPosition { get; set; }
     public List<MapNode3> CurrentChoices { get; set; }
 
-    public void SetMap(GameMap3 map, int totalNodeCount)
+    public void SetMap(GameMap3 map)
     {
         CurrentMap = map;
         CompletedNodes = new List<MapNodeType>();
