e591b73a1479599b3b80e31d410fb281c88b9bb3
noah reinagel <noah.r9@gmail.com>
2021-06-28 17:14:22 -0700

    *added penetration tree

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/TargetHasNoShields.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/TargetHasNoShields")]
+public class TargetHasNoShields : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.Target.Members.All(m => m.CurrentShield() <= 0)
+            ? Maybe<string>.Missing()
+            : new Maybe<string>($"Some of the targets have shields");
+    }
+}
---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/TargetHasShields.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/TargetHasShields")]
+public class TargetHasShields : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.Target.Members.All(m => m.CurrentShield() > 0)
+            ? Maybe<string>.Missing()
+            : new Maybe<string>($"Some of the targets have no shields");
+    }
+}
