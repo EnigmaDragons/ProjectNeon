532dcac7ad864e59f8f0a73976d1c6df38e209c2
noah reinagel <noah.r9@gmail.com>
2021-07-09 21:27:34 -0700

    added corps class

---
+++ src/ProjectNeon/Assets/Features/Corps/Corp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameContent/Corp")]
+public class Corp : ScriptableObject
+{
+    [SerializeField] private string name;
+    [SerializeField] private Sprite logo;
+    [SerializeField] private Color color;
+
+    public string Name => name;
+    public Sprite Logo => logo;
+    public Color Color => color;
+}
