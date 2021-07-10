6b734c27d81d9016d2139402dd27889b38798b2b
SilasReinagel <silas.reinagel@gmail.com>
2021-07-10 10:28:56 -0700

    Coding: Doesn't allow Party Corp Affinity interest in more than 2 Corps. Closes #3282

--- src/ProjectNeon/Assets/Features/Corps/PartyCorpAffinityCalculator.cs
+++ src/ProjectNeon/Assets/Features/Corps/PartyCorpAffinityCalculator.cs
@@ -10,32 +10,44 @@ public static class PartyCorpAffinityCalculator
             return new PartyCorpAffinity();
         
         var corpEquipmentCounts = equipment.GroupBy(e => e.Corp).ToDictionary(e => e.Key, e => e.Count());
-        var corpEquipmentsCount = allCorps.ToDictionary(c => c.Key, c => corpEquipmentCounts.ValueOrDefault(c.Key, 0));
+        var allCorpEquipmentsCount = allCorps.ToDictionary(c => c.Key, c => corpEquipmentCounts.ValueOrDefault(c.Key, 0));
         var highestCount = corpEquipmentCounts.OrderByDescending(e => e.Value).First().Value;
         var exclusiveHighestCorp = corpEquipmentCounts.Count(x => x.Value == highestCount) > 1
             ? "None"
             : corpEquipmentCounts.First(x => x.Value == highestCount).Key;
+        var allCorpGearTiers = allCorpEquipmentsCount.ToDictionary(c => c.Key, c => GearTier(equipmentSlots, c.Value));
         
-        var baseAffinities = corpEquipmentsCount.ToDictionary(
-            e => e.Key, 
-            e => AffinityStrength(equipmentSlots, e.Value, exclusiveHighestCorp.Equals(e.Key)));
+        var baseAffinities = allCorpGearTiers.ToDictionary(
+            c => c.Key, 
+            c => AffinityStrength(c.Value, exclusiveHighestCorp.Equals(c.Key), allCorpGearTiers));
 
-        var adjustedAffinities = baseAffinities.ToDictionary(
+        var adjustedAffinitiesForRivalries = baseAffinities.ToDictionary(
             e => e.Key,
             e => AdjustedBasedOnRivalAffinity(e.Value,
                 allCorps[e.Key].RivalCorpNames
                     .Select(r => baseAffinities.ValueOrDefault(r, CorpAffinityStrength.None))));
 
-        return new PartyCorpAffinity(adjustedAffinities);
+        return new PartyCorpAffinity(adjustedAffinitiesForRivalries);
     }
 
-    private static CorpAffinityStrength AffinityStrength(int equipmentSlots, int count, bool isExclusiveHighest)
+    private static int GearTier(int equipmentSlots, int count)
     {
-        if (count >= equipmentSlots / 3 && isExclusiveHighest)
-            return CorpAffinityStrength.Fanatic;
-        else if (count >= equipmentSlots / 5 && isExclusiveHighest)
-            return CorpAffinityStrength.Loyal;
+        if (count >= equipmentSlots / 3)
+            return 3;
+        else if (count >= equipmentSlots / 5)
+            return 2;
         else if (count > 0)
+            return 1;
+        return 0;
+    }
+
+    private static CorpAffinityStrength AffinityStrength(int gearTier, bool isExclusiveHighest, Dictionary<string, int> corpGearTiers)
+    {
+        if (gearTier == 3 && isExclusiveHighest)
+            return CorpAffinityStrength.Fanatic;
+        else if (gearTier == 2 && isExclusiveHighest)
+            return CorpAffinityStrength.Loyal;
+        else if (gearTier > 0 && (isExclusiveHighest || corpGearTiers.Count(c => c.Value >= gearTier) < 3)) // You can be interested in 2 Corps simultaneously, but not 3.
             return CorpAffinityStrength.Interested;
         return CorpAffinityStrength.None;
     }
--- src/ProjectNeon/Assets/Z_Tests/Features/Corps/PartyCorpAffinityCalculatorTests.cs
+++ src/ProjectNeon/Assets/Z_Tests/Features/Corps/PartyCorpAffinityCalculatorTests.cs
@@ -7,11 +7,13 @@ public class PartyCorpAffinityCalculatorTests
     private static InMemoryCorp RivalToCorp1 = new InMemoryCorp { Name = "RivalToCorp1", RivalCorpNames = "Corp1".AsArray() };
     private static InMemoryCorp Corp1 = new InMemoryCorp { Name = "Corp1", RivalCorpNames = RivalToCorp1.Name.AsArray() };
     private static InMemoryCorp Corp2 = new InMemoryCorp { Name = "Corp2" };
+    private static InMemoryCorp Corp3 = new InMemoryCorp { Name = "Corp3" };
 
     private readonly Dictionary<string, Corp> _allCorps = new Dictionary<string, Corp>
     {
         { Corp1.Name, Corp1 },
         { Corp2.Name, Corp2 },
+        { Corp3.Name, Corp3 },
         { RivalToCorp1.Name, RivalToCorp1 }
     };
 
@@ -45,6 +47,35 @@ public class PartyCorpAffinityCalculatorTests
         
         Assert.AreEqual(CorpAffinityStrength.Interested, affinity[Corp1]);
         Assert.AreEqual(CorpAffinityStrength.Interested, affinity[Corp2]);
+    }
+    
+    [Test]
+    public void PartyCorpAffinityCalculator_ThreeEquipmentsOfAllDifferentCorps_NotInterestedInAnyCorps()
+    {
+        var affinity = GetAffinity(new List<Equipment>
+        {
+            new InMemoryEquipment { Corp = Corp1 },
+            new InMemoryEquipment { Corp = Corp2 },
+            new InMemoryEquipment { Corp = Corp3 },
+        });
+        
+        Assert.IsTrue(affinity.All(x => x.Value == CorpAffinityStrength.None));
+    }
+    
+    [Test]
+    public void PartyCorpAffinityCalculator_TwoEquipmentsOfOneCorpWithStrayGearFromOthers_OnlyInterestedInCorp1()
+    {
+        var affinity = GetAffinity(new List<Equipment>
+        {
+            new InMemoryEquipment { Corp = Corp1 },
+            new InMemoryEquipment { Corp = Corp1 },
+            new InMemoryEquipment { Corp = Corp2 },
+            new InMemoryEquipment { Corp = Corp3 },
+        });
+        
+        Assert.AreEqual(CorpAffinityStrength.Interested, affinity[Corp1]);
+        Assert.AreEqual(CorpAffinityStrength.None, affinity[Corp2]);
+        Assert.AreEqual(CorpAffinityStrength.None, affinity[Corp3]);
     }
     
     [Test]
