5de168d4cfb12dd7106366be50522f207d79335f
SilasReinagel <silas.reinagel@gmail.com>
2021-07-09 23:34:00 -0700

    Rebalance: Removed Shop Pricing Variance. Closes #3261

--- src/ProjectNeon/Assets/Features/Shopping/CardShopPricing.cs
+++ src/ProjectNeon/Assets/Features/Shopping/CardShopPricing.cs
@@ -7,31 +7,32 @@ public static class CardShopPricing
     public static int CardShopPrice(this Rarity rarity)
     {
         if (rarity == Rarity.Common)
-            return WithShopPricingVariance(40);
+            return 40;
         if (rarity == Rarity.Uncommon)
-            return WithShopPricingVariance(80);
+            return 80;
         if (rarity == Rarity.Rare)
-            return WithShopPricingVariance(160);
+            return 160;
         if (rarity == Rarity.Epic)
-            return WithShopPricingVariance(320);
-        return WithShopPricingVariance(25);
+            return 320;
+        return 25;
     }
 
     public static int EquipmentShopPrice(this Rarity rarity, float priceFactor)
     {
         if (rarity == Rarity.Common)
-            return WithShopPricingVariance(80 * priceFactor);
+            return Int(80 * priceFactor);
         if (rarity == Rarity.Uncommon)
-            return WithShopPricingVariance(200 * priceFactor);
+            return Int(200 * priceFactor);
         if (rarity == Rarity.Rare)
-            return WithShopPricingVariance(400 * priceFactor);
+            return Int(400 * priceFactor);
         if (rarity == Rarity.Epic)
-            return WithShopPricingVariance(600 * priceFactor);
-        return WithShopPricingVariance(50);
+            return Int(600 * priceFactor);
+        return 50;
     }
 
     public static int WithShopPricingVariance(this int target)
         => WithShopPricingVariance(Convert.ToSingle(target));
     public static int WithShopPricingVariance(this float target) 
         => Rng.Int((target * 0.8f).FlooredInt(), (target * 1.2f).CeilingInt());
+    public static int Int(float f) => f.CeilingInt();
 }
