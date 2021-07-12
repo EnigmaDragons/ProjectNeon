f362aa880eeb98203d96733ee7983f08f6c1230c
noah reinagel <noah.r9@gmail.com>
2021-07-11 19:16:22 -0700

    *fixed clinic cost calculation

--- src/ProjectNeon/Assets/Features/Corps/HealthDependantClinicCostCalculator.cs
+++ src/ProjectNeon/Assets/Features/Corps/HealthDependantClinicCostCalculator.cs
@@ -2,7 +2,7 @@
 
 public class HealthDependantClinicCostCalculator : ClinicCostCalculator
 {
-    public int GetFullHealCost(Hero hero) => (int)Math.Floor(((float)hero.CurrentHp / (float)hero.Stats.MaxHp()) * 60f);
+    public int GetFullHealCost(Hero hero) => (int)Math.Floor((1 -((float)hero.CurrentHp / (float)hero.Stats.MaxHp())) * 60f);
     public int GetInjuryHealCost() => 30;
     public void RequestClinicHealService() { }
 }
