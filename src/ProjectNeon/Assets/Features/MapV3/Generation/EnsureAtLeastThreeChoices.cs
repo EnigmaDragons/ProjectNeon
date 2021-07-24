ce97ab0ac0452136663424a69b8021c58b35b7d6
noah reinagel <noah.r9@gmail.com>
2021-07-23 20:53:43 -0700

    *Fixed it so Story Event Nodes don't show up and there is always at least 3 nodes and increased the odds of various things showing up

---
+++ src/ProjectNeon/Assets/Features/MapV3/Generation/EnsureAtLeastThreeChoices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class EnsureAtLeastThreeChoices : MapGenerationRule3
+{
+    public List<MapNode3> FilterNodeTypes(List<MapNode3> list, CurrentGameMap3 map, PartyAdventureState party, AdventureProgress2 progress)
+    {
+        while (list.Count < 3)
+            list.Add(new MapNode3 { Type = MapNodeType.Combat });
+        return list;
+    }
+}
