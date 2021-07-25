4caf82c564bad05c738eb8097ab5dc8dd55a6480
SilasReinagel <silas.reinagel@gmail.com>
2021-07-25 11:59:23 -0700

    UI: Added In Game Menu with Main Menu and Exit Game buttons. Closes #3467. Closes #2293. Closes #2283. Closes #3466

--- src/ProjectNeon/Assets/Core/UI/PauseGameWhileActive.cs
+++ src/ProjectNeon/Assets/Core/UI/PauseGameWhileActive.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public sealed class PauseGameWhileActive : MonoBehaviour
 {
+    private float _lastTimeScale = 1;
+    
     private void OnEnable()
     {
+        _lastTimeScale = Time.timeScale;
         Time.timeScale = 0;
         Message.Publish(new GamePaused());
     }
 
     private void OnDisable()
     {
-        Time.timeScale = 1;
+        Time.timeScale = _lastTimeScale;
         Message.Publish(new GameContinued());
     }
 }
