d7a8be4c676ae743c1fb7bc91b2cc597ebfd1c67
noah reinagel <noah.r9@gmail.com>
2021-08-07 14:09:03 -0700

    Fixed AIs cheating for both taunt and stealth mechanics for targeting in their strategy closes #3512

--- src/ProjectNeon/Assets/Features/BattleV2/AIs/AIStrategyGenerator.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/AIs/AIStrategyGenerator.cs
@@ -13,17 +13,20 @@ public sealed class AIStrategyGenerator
     public AIStrategy Generate(BattleState s, CardTypeData disabledCard)
     {
         var relevantEnemies = _forTeam == TeamType.Enemies 
-            ? s.Heroes.Where(h => h.IsConscious()) 
-            : s.EnemyMembers.Where(e => e.IsConscious());
-
-        var vulnerableEnemies = relevantEnemies.Where(e => e.IsVulnerable()).ToArray();
-        var tauntEnemies = relevantEnemies.Where(e => e.HasTaunt());
+            ? s.Heroes.Where(h => h.IsConscious()).ToArray() 
+            : s.EnemyMembers.Where(e => e.IsConscious()).ToArray();
+        
+        var targetableEnemies = relevantEnemies.Where(x => !x.IsStealthed()).ToArray();
+        var vulnerableEnemies = targetableEnemies.Where(e => e.IsVulnerable()).ToArray();
+        var tauntEnemies = targetableEnemies.Where(e => e.HasTaunt()).ToArray();
         var preferredSingleTarget =
             tauntEnemies.Any() 
                 ? tauntEnemies.Random()
                 : vulnerableEnemies.Any() 
                     ? vulnerableEnemies.Random() 
-                    : relevantEnemies.Random();
+                    : targetableEnemies.Any()
+                        ? targetableEnemies.Random()
+                        : Maybe<Member>.Missing();
         
         var team = _forTeam == TeamType.Enemies
                    ? s.EnemyMembers.Where(h => h.IsConscious()) 
@@ -37,5 +40,23 @@ public sealed class AIStrategyGenerator
             .First();
         
         return new AIStrategy(preferredSingleTarget, new Multiple(relevantEnemies.ToArray()), designatedAttacker, disabledCard);
+    }
+
+    public AIStrategy Update(AIStrategy strategy, BattleState s)
+    {
+        if (strategy.SingleMemberAttackTarget.IsPresent && strategy.SingleMemberAttackTarget.Value.IsConscious() && !strategy.SingleMemberAttackTarget.Value.IsStealthed()) 
+            return strategy;
+        var relevantEnemies = _forTeam == TeamType.Enemies 
+            ? s.Heroes.Where(h => h.IsConscious()).ToArray() 
+            : s.EnemyMembers.Where(e => e.IsConscious()).ToArray();
+        var targetableEnemies = relevantEnemies.Where(x => !x.IsStealthed()).ToArray();
+        var tauntEnemies = targetableEnemies.Where(e => e.HasTaunt()).ToArray();
+        var preferredSingleTarget =
+            tauntEnemies.Any() 
+                ? tauntEnemies.Random()
+                : targetableEnemies.Any()
+                    ? targetableEnemies.Random()
+                    : Maybe<Member>.Missing();
+        return new AIStrategy(preferredSingleTarget, strategy.GroupAttackTarget, strategy.DesignatedAttacker, strategy.DisabledCard);
     }
 }
