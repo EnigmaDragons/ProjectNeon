688ff486ef8385b20a68c74f6405ff0c2c2c4ed3
SilasReinagel <silas.reinagel@gmail.com>
2021-08-21 10:05:04 -0700

    UI: Setup Music and Sound volume controls. Closes #3655. Closes #3653

--- src/ProjectNeon/Assets/Core/Audio/AudioClipVolume.cs
+++ src/ProjectNeon/Assets/Core/Audio/AudioClipVolume.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [Serializable]
 public class AudioClipVolume
 {
+    [SerializeField] private string name;
     public AudioClip clip;
     public float volume = 1f;
+
+    public string Name => string.IsNullOrWhiteSpace(name) ? clip.name : name;
 }
