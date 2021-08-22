f453f8824d7d4730a82165aa990e685727e3bbf8
noah reinagel <noah.r9@gmail.com>
2021-08-22 15:47:45 -0700

    Added Medigeneix Drug Samples Event

---
+++ src/ProjectNeon/Assets/Features/StoryEvents/Rewards/SpecificCardReward.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "StoryEvent/Rewards/Specific Card")]
+public class SpecificCardReward : StoryResult
+{
+    [SerializeField] private CardType card;
+
+    public override int EstimatedCreditsValue => card.Rarity.CardShopPrice();
+    
+    public override void Apply(StoryEventContext ctx)
+    {
+        ctx.Party.Add(card);
+        Message.Publish(new ShowCardReward(card));
+    }
+
+    public override void Preview()
+    {
+        Message.Publish(new ShowCardResultPreview { Card = card });
+    }
+}
