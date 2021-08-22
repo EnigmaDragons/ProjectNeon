43c6996b9a4e11d0ca785b10e7b59e035d5dfe39
noah reinagel <noah.r9@gmail.com>
2021-08-22 05:56:32 -0700

    Added corp to story events

--- src/ProjectNeon/Assets/Features/StoryEvents/V2/StoryEvent2.cs
+++ src/ProjectNeon/Assets/Features/StoryEvents/V2/StoryEvent2.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class StoryEvent2 : ScriptableObject
 {
     [SerializeField] private StorySetting settingType;
+    [SerializeField] private StaticCorp corp;
     [SerializeField, TextArea(4, 10)] private string storyText;
     [SerializeField] private StoryEventChoice2[] choices;
 
     public StorySetting StorySetting => settingType;
+    public StaticCorp Corp => corp;
     public string StoryText => storyText;
     public StoryEventChoice2[] Choices => choices.ToArray();
 }
