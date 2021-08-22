1a3542eee8e6a633e30d173a2aede9ae5a28614f
SilasReinagel <silas.reinagel@gmail.com>
2021-08-22 07:39:35 -0700

    UI: Story Event multi-results look better. Simplified Outcome experience. Closes #3710. Closes #3715

--- src/ProjectNeon/Assets/Features/StoryEvents/Results/AndResult.cs
+++ src/ProjectNeon/Assets/Features/StoryEvents/Results/AndResult.cs
@@ -7,7 +7,6 @@ public class AndResult : StoryResult
     [SerializeField] private StoryResult first;
     [SerializeField] private StoryResult second;
     [SerializeField] private bool isReward;
-    [SerializeField] private string text;
     [SerializeField] private string previewText;
     public override int EstimatedCreditsValue => first.EstimatedCreditsValue + second.EstimatedCreditsValue;
     public override bool IsReward => isReward;
@@ -16,7 +15,6 @@ public class AndResult : StoryResult
     {
         first.Apply(ctx);
         second.Apply(ctx);
-        Message.Publish(new ShowStoryEventResultMessage(text));
     }
 
     public override void Preview()
