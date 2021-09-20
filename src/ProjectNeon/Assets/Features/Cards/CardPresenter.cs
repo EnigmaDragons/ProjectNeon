162c992286c3d0ddc6855235c414be9767082388
Noah Reinagel <noah.r9@gmail.com>
2021-09-19 17:15:01 -0700

    *Fixed resource intropolation and X costers and a few cards with errors on them

--- src/ProjectNeon/Assets/Features/Cards/CardPresenter.cs
+++ src/ProjectNeon/Assets/Features/Cards/CardPresenter.cs
@@ -343,7 +343,7 @@ public class CardPresenter : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         nameLabel.text = _cardType.Name;
         description.text = !shouldUseLibraryMode
             ? _cardType.InterpolatedDescription(_card.Owner, _card.LockedXValue.OrDefault(() => _card.Owner.CalculateResources(_card.Type).XAmountQuantity)) 
-            : _cardType.InterpolatedDescription(Maybe<Member>.Missing(), ResourceQuantity.None);
+            : _cardType.InterpolatedDescription(_card.Owner, ResourceQuantity.None);
         type.text = _cardType.ArchetypeDescription();
         art.sprite = _cardType.Art;
         rarity.Set(_cardType.Rarity);
