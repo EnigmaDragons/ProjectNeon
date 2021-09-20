c934ba82a5c2403a44a576a7b10075f1daa53027
SilasReinagel <silas.reinagel@gmail.com>
2021-09-19 17:18:01 -0700

    Tutorial: Card Detailed Info. Closes #3897

--- src/ProjectNeon/Assets/Features/Cards/UI/InitCardPresenterOnStart.cs
+++ src/ProjectNeon/Assets/Features/Cards/UI/InitCardPresenterOnStart.cs
@@ -5,9 +5,12 @@ public class InitCardPresenterOnStart : MonoBehaviour
     [SerializeField] private CardPresenter cardPresenter;
     [SerializeField] private CardType card;
     [SerializeField] private BaseHero owner;
+    [SerializeField] private bool showComprehensiveInfo = false;
     
     private void Start()
     {
         cardPresenter.Set(new Card(-1, new Hero(owner, new RuntimeDeck()).AsMember(1), card, owner.Tint, owner.Bust));
+        if (showComprehensiveInfo)
+            cardPresenter.ShowComprehensiveCardInfo();
     }
 }
