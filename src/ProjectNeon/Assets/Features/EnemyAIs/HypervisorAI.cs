b44c3d989e99a944aa94e6d52293676849d5bacb
Noah Reinagel <noah.r9@gmail.com>
2021-10-16 17:24:55 -0700

    Enemy: Hyporvisor rework - Now dedicated to giving resources and vulnerable

--- src/ProjectNeon/Assets/Features/EnemyAIs/HypervisorAI.cs
+++ src/ProjectNeon/Assets/Features/EnemyAIs/HypervisorAI.cs
@@ -10,7 +10,6 @@ public class HypervisorAI : StatelessTurnAI
             .IfTrueDontPlayType(c => c.Allies.None(x => x.CurrentHp() + x.CurrentShield() > x.MaxHp() * 0.5f), CardTag.BuffAttack, CardTag.Ultimate)
             .WithSelectedUltimateIfAvailable()
             .IfTrueDontPlayType(c => c.Allies.Length > 1, CardTag.Attack)
-            .IfTrueDontPlayType(c => c.Allies.Length < 3, CardTag.Group, CardTag.BuffAttack)
             .WithCommonSenseSelections()
             .WithFinalizedCardSelection()
             .WithSelectedTargetsPlayedCard();
