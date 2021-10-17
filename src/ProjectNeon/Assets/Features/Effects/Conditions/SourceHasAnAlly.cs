ab8b6fe8e1f5077554237a6cac2e945d582dc996
Noah Reinagel <noah.r9@gmail.com>
2021-10-16 22:16:34 -0700

    Enemy: Anton Boss

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/SourceHasAnAlly.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "EffectConditions/SourceHasAnAlly")]
+public class SourceHasAnAlly : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.BattleMembers.Any(x => x.Value.IsConscious() && x.Value.TeamType == ctx.Source.TeamType && x.Value.Id != ctx.Source.Id)
+            ? Maybe<string>.Missing()
+            : new Maybe<string>($"{ctx.Source.Name} has no ally");
+    }
+}
---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/SourceHasNoDodge.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/SourceHasNoDodge")]
+public class SourceHasNoDodge : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.Source.Dodge() > 0
+            ? new Maybe<string>($"{ctx.Source.Name} has dodge")
+            : Maybe<string>.Missing();
+    }
+}
