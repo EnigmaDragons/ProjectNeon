952e0eafc19db3b621bee6a1f011be92ef9dc3fc
noah reinagel <noah.r9@gmail.com>
2021-10-24 02:27:44 -0700

    *wired up more animation types

--- src/ProjectNeon/Assets/Features/Combatants/Animations/CharacterAnimationType.cs
+++ src/ProjectNeon/Assets/Features/Combatants/Animations/CharacterAnimationType.cs
@@ -3,4 +3,6 @@
     Crouch = 0,
     Slash = 1,
     Stab = 2,
+    TargetSingleEnemy = 3,
+    TargetTeam = 4,
 }
--- src/ProjectNeon/Assets/Features/Combatants/Animations/CharacterAnimations.cs
+++ src/ProjectNeon/Assets/Features/Combatants/Animations/CharacterAnimations.cs
@@ -10,6 +10,8 @@ public class CharacterAnimations
     [SerializeField] private CharacterAnimation crouch;
     [SerializeField] private CharacterAnimation slash;
     [SerializeField] private CharacterAnimation stab;
+    [SerializeField] private CharacterAnimation targetSingleEnemy;
+    [SerializeField] private CharacterAnimation targetTeam;
     
     public DictionaryWithDefault<CharacterAnimationType, CharacterAnimationStep[]> AnimationMap 
         => new DictionaryWithDefault<CharacterAnimationType, CharacterAnimationStep[]>(new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } })
@@ -17,5 +19,7 @@ public class CharacterAnimations
             { CharacterAnimationType.Crouch, crouch?.Steps ?? new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } } },
             { CharacterAnimationType.Slash, slash?.Steps ?? new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } } },
             { CharacterAnimationType.Stab, stab?.Steps ?? new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } } },
+            { CharacterAnimationType.TargetSingleEnemy, targetSingleEnemy?.Steps ?? new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } } },
+            { CharacterAnimationType.TargetTeam, targetTeam?.Steps ?? new [] { new CharacterAnimationStep { StepType = CharacterAnimationStepType.PublishFinished } } },
         };
 }
