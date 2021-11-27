ef9b54d526104e83360c79074827327d79bde15f
SilasReinagel <silas.reinagel@gmail.com>
2021-11-26 23:06:23 -0700

    Coding: Hero Level Up Permanent Guarantees A Matching Archetype Augment. Closes #4514

--- src/ProjectNeon/Assets/Features/Party/LevelUps/HeroPermanentAugmentOptions.cs
+++ src/ProjectNeon/Assets/Features/Party/LevelUps/HeroPermanentAugmentOptions.cs
@@ -9,8 +9,17 @@ public class HeroPermanentAugmentOptions : LevelUpOptions
     
     public override LevelUpOption[] Generate(Hero h)
     {
-        var augments = allEquipmentPool.Random(EquipmentSlot.Augmentation, rarity, h.Character.AsArray(), 3);
-        var options = augments.Select(a => (LevelUpOption)new AugmentLevelUpOption(a)).ToArray();
-        return options;
+        var archetypes = h.Archetypes;
+        var archMatchingAugment = allEquipmentPool.All
+            .Where(e => e.Rarity == rarity 
+                        && e.Slot == EquipmentSlot.Augmentation 
+                        && e.Archetypes.Any(a => archetypes.Contains(a)))
+            .TakeRandom(1);
+        var additionalAugments = allEquipmentPool.Random(EquipmentSlot.Augmentation, rarity, h.Character.AsArray(), 3 - archMatchingAugment.Length);
+        var options = archMatchingAugment
+            .Concat(additionalAugments)
+            .Select(a => (LevelUpOption)new AugmentLevelUpOption(a))
+            .ToArray();
+        return options.Shuffled();
     }
 }
