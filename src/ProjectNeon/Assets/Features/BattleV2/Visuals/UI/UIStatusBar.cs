50b64527ccc74d455595bb8892be8bfb7dc7df52
SilasReinagel <silas.reinagel@gmail.com>
2021-12-11 08:34:10 -0700

    Bug Fix: UI Status Bar run ender with too many statuses. Closes #4637

--- src/ProjectNeon/Assets/Features/BattleV2/Visuals/UI/UIStatusBar.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Visuals/UI/UIStatusBar.cs
@@ -20,7 +20,7 @@ public sealed class UIStatusBar : StatusBar
     {
         for (var i = 0; i < Math.Max(statuses.Count, _icons.Count); i++)
         {
-            if (i < statuses.Count)
+            if (i < Math.Min(statuses.Count, _icons.Count))
                 _icons[i].Show(statuses[i]);
             else
                 _icons[i].gameObject.SetActive(false);
