14ec781c03c5ef7e4aae6d73438945c13a6f99bc
Noah Reinagel <noah.r9@gmail.com>
2022-01-09 04:51:39 -0700

    *added new Anton boss and adjusted end

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/TargetHasAGlitchedCardInHand.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/TargetHasAGlitchedCardInHand")]
+public class TargetHasAGlitchedCardInHand : StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.PlayerCardZones.HandZone.Cards.Any(x => x.Mode == CardMode.Glitched && x.Owner.Id == ctx.Target.Members[0].Id)
+            ? Maybe<string>.Missing()
+            : new Maybe<string>($"No cards in player's hand are glitched");
+    }
+}
---
+++ src/ProjectNeon/Assets/Features/EnemyAIs/NotnaAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/Notna")]
+public class NotnaAI : StatelessTurnAI
+{
+    protected override IPlayedCard Select(int memberId, BattleState battleState, AIStrategy strategy)
+    {
+        return new CardSelectionContext(memberId, battleState, strategy)
+            .WithSelectedDesignatedAttackerCardIfApplicable()
+            .IfTrueDontPlayType(x => true, CardTag.Attack)
+            .WithCommonSenseSelections()
+            .IfTrueDontPlayType(x => x.Zones.HandZone.Cards.All(x => x.Mode != CardMode.Glitched), CardTag.Ultimate)
+            .WithSelectedUltimateIfAvailable()
+            .WithFinalizedCardSelection()
+            .WithSelectedTargetsPlayedCard();
+    }
+}
