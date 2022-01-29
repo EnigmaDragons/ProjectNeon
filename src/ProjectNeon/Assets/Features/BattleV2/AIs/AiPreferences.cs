7ccbfd71538cb703d8d46e051346f17faf628000
SilasReinagel <silas.reinagel@gmail.com>
2022-01-29 13:32:41 -0700

    Coding: Enemy Specialist prefer not to Attack by default. Closes #4985

--- src/ProjectNeon/Assets/Features/BattleV2/AIs/AiPreferences.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/AIs/AiPreferences.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 [Serializable]
 public class AiPreferences
@@ -7,4 +8,24 @@ public class AiPreferences
     public CardTag[] RotatePlayingCardTags = new CardTag[0];
     public CardTag[] UnpreferredCardTags = new CardTag[0];
     public DesignatedAttackerPriority DesignatedAttackerPriority = DesignatedAttackerPriority.Default;
+
+    public AiPreferences WithDefaultsBasedOnRole(BattleRole role)
+    {
+        if (role == BattleRole.Specialist)
+            return CopyWith(x => x.UnpreferredCardTags = UnpreferredCardTags.Append(CardTag.Attack).ToHashSet().ToArray());
+        return this;
+    }
+
+    private AiPreferences CopyWith(Action<AiPreferences> update)
+    {
+        var clone = new AiPreferences
+        {
+            CardTagPriority = CardTagPriority,
+            RotatePlayingCardTags = RotatePlayingCardTags,
+            UnpreferredCardTags = UnpreferredCardTags.Append(CardTag.Attack).ToHashSet().ToArray(),
+            DesignatedAttackerPriority = DesignatedAttackerPriority
+        };
+        update(clone);
+        return clone;
+    }
 }
