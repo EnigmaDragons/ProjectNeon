00a15e710e207af94c77e1f16089a22991409d3d
Noah Reinagel <noah.r9@gmail.com>
2022-01-29 17:29:34 -0700

    UI: Enemy Beastiary shows cards in a better order and gives you some important additional information

--- src/ProjectNeon/Assets/Features/Cards/UI/SimpleCardsView.cs
+++ src/ProjectNeon/Assets/Features/Cards/UI/SimpleCardsView.cs
@@ -7,12 +7,18 @@ public class SimpleCardsView : MonoBehaviour
     [SerializeField] private CardPresenter[] cardPresenters;
 
     public int MaxCardsDisplayed => cardPresenters.Length;
-    
-    public void Show(IEnumerable<Card> cards)
+
+    public void Show(IEnumerable<Card> cards) => Show(cards.Select(x => (x, "")));
+
+    public void Show(IEnumerable<(Card, string)> cards)
     {
         cardPresenters.ForEach(card => card.gameObject.SetActive(false));
         var c = cards.ToArray();
-        for(var i = 0; i < cardPresenters.Length && i < c.Length; i++)
-            cardPresenters[i].Set(c[i]);
+        for (var i = 0; i < cardPresenters.Length && i < c.Length; i++)
+        {
+            cardPresenters[i].Set(c[i].Item1);
+            if (!string.IsNullOrWhiteSpace(c[i].Item2))
+                cardPresenters[i].ShowEnemyCareType(c[i].Item2);
+        }
     }
 }
