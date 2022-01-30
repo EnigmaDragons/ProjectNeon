808651ad87e2ed4caf59d4d789b99623834d88b1
SilasReinagel <silas.reinagel@gmail.com>
2022-01-29 17:05:38 -0700

    Coding: AI Added Turn Exclusive Card options. Closes #4995. Closes #4990

--- src/ProjectNeon/Assets/Features/BattleV2/AIs/StatefulTurnAI.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/AIs/StatefulTurnAI.cs
@@ -5,19 +5,19 @@ public abstract class StatefulTurnAI : TurnAI
     public abstract override void InitForBattle();
     protected abstract IPlayedCard Select(int memberId, BattleState battleState, AIStrategy strategy);
     protected abstract void TrackState(IPlayedCard card, BattleState state, AIStrategy strategy);
-
+    
     public sealed override IPlayedCard Play(int memberId, BattleState battleState, AIStrategy strategy)
         => WithTrackedState(SafeSelect(memberId, battleState, strategy), battleState, strategy);
 
     public sealed override IPlayedCard Anticipate(int memberId, BattleState battleState, AIStrategy strategy)
         => SafeSelect(memberId, battleState, strategy.AnticipationCopy);
-    
+
     private IPlayedCard WithTrackedState(IPlayedCard card, BattleState state, AIStrategy strategy)
     {
         TrackState(card, state, strategy);
         return card;
     }
-    
+
     private IPlayedCard SafeSelect(int memberId, BattleState battleState, AIStrategy strategy)
     {
         try
--- src/ProjectNeon/Assets/Features/EnemyAIs/GeneralAI.cs
+++ src/ProjectNeon/Assets/Features/EnemyAIs/GeneralAI.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "AI/GeneralAI")]
 public class GeneralAI : StatefulTurnAI
 {
+    private int _currentTurnNumber;
+    
     private Dictionary<int, IPlayedCard> _lastPlayedCard = new Dictionary<int, IPlayedCard>();
     private Dictionary<int, int> _focusTargets = new Dictionary<int, int>();
-
+    private Dictionary<int, List<IPlayedCard>> _currentTurnPlayed = new Dictionary<int, List<IPlayedCard>>();
+    
     public override void InitForBattle()
     {
         _lastPlayedCard = new Dictionary<int, IPlayedCard>();
         _focusTargets = new Dictionary<int, int>();
+        _currentTurnPlayed = new Dictionary<int, List<IPlayedCard>>();
+    }
+
+    public IPlayedCard Play(int turnNumber, CardSelectionContext ctx)
+    {
+        var selected = Select(turnNumber, ctx);
+        TrackState(selected);
+        return selected;
+    }
+        
+    private IPlayedCard Select(int turnNumber, CardSelectionContext ctx)
+    {
+        UpdateTurnTracking(turnNumber);
+        
+        return ctx
+            .WithCommonSenseSelections()
+            .IfTrueDontPlayType(_ => _currentTurnPlayed.ValueOrMaybe(ctx.Member.Id).IsPresentAnd(cards => cards.Any(c => c.Card.Is(CardTag.Exclusive))), CardTag.Exclusive)
+            .WithSelectedFocusCardIfApplicable()
+            .WithSelectedDesignatedAttackerCardIfApplicable()
+            .WithFinalizedSmartCardSelection()
+            .WithSelectedTargetsPlayedCard();
     }
 
     protected override IPlayedCard Select(int memberId, BattleState battleState, AIStrategy strategy)
@@ -19,18 +43,20 @@ public class GeneralAI : StatefulTurnAI
         var me = battleState.Members[memberId];
         var focusTarget = GetFocusTarget(me, battleState);
         var lastPlayedCard = _lastPlayedCard.ValueOrMaybe(memberId).Map(c => c.Card.Type);
-        return new CardSelectionContext(me, battleState, strategy, focusTarget, lastPlayedCard)
-            .WithCommonSenseSelections()
-            .WithSelectedFocusCardIfApplicable()
-            .WithSelectedDesignatedAttackerCardIfApplicable()
-            .WithFinalizedSmartCardSelection()
-            .WithSelectedTargetsPlayedCard();
+        
+        return Select(battleState.TurnNumber, new CardSelectionContext(me, battleState, strategy, focusTarget, lastPlayedCard));
     }
 
     protected override void TrackState(IPlayedCard card, BattleState state, AIStrategy strategy)
+        => TrackState(card);
+    
+    private void TrackState(IPlayedCard card)
     {
         var memberId = card.MemberId();
         _lastPlayedCard[memberId] = card;
+        if (!_currentTurnPlayed.ContainsKey(memberId))
+            _currentTurnPlayed[memberId] = new List<IPlayedCard>();
+        _currentTurnPlayed[memberId].Add(card);
         if (card.Card.Is(CardTag.Focus))
             _focusTargets[memberId] = card.PrimaryTargetId();
     }
@@ -53,5 +79,14 @@ public class GeneralAI : StatefulTurnAI
         }
 
         return maybeMember;
+    }
+    
+    private void UpdateTurnTracking(int currentTurnNumber)
+    {
+        if (currentTurnNumber == _currentTurnNumber)
+            return;
+
+        _currentTurnNumber = currentTurnNumber;
+        _currentTurnPlayed = new Dictionary<int, List<IPlayedCard>>();
     }
 }
