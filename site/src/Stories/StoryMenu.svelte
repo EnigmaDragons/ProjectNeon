91ab0d7cb744e4852d2431ac722d724ebce3e1e3
SilasReinagel <silas.reinagel@gmail.com>
2022-02-11 23:24:04 -0700

    Project: Finished new short story. Closes #4954

---
+++ site/src/Stories/StoryMenu.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Container from '../Elements/Container.svelte';
+</script>
+
+<Container>
+  <div class="column center-vh">
+    <h1>Stories of Metroplex Zero</h1>
+    <a href="/index.html?page=story-welcome-to-zantocorp">1. Welcome To ZantoCorp</a>
+    <a href="/index.html?page=story-vopvana">2. Legacy of Vopvana Tech</a>
+  </div>
+</Container>
+
+<style>
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    font-size: 1.4em;
+    margin-bottom: 0.2em;
+  }
+</style>
