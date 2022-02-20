19101401886cc6ede7987aea0d0080ff489a02be
SilasReinagel <silas.reinagel@gmail.com>
2022-02-20 00:04:34 -0700

    Art: Updated Lyssa Art. Closes #5196. Closes #4573

--- src/ProjectNeon/Assets/Features/GearCollection/FullGearLibraryUI.cs
+++ src/ProjectNeon/Assets/Features/GearCollection/FullGearLibraryUI.cs
@@ -21,9 +21,9 @@ public class FullGearLibraryUI : MonoBehaviour
             gearPool.All
                 .Where(c => c.Slot != EquipmentSlot.Permanent)
                 .Where(c => !c.IsRandomlyGenerated())
-                .OrderByDescending(c => c.Corp)
-                .ThenByDescending(c => c.Archetypes.Any())
+                .OrderByDescending(c => c.Archetypes.Any())
                 .ThenBy(c => c.GetArchetypeKey())
+                .ThenBy(c => c.Corp)
                 .ThenBy(c => c.Slot)
                 .ThenBy(c => c.Rarity)
                 .ThenBy(c => c.Name)
