fbb525f60f5bbf40fd8ee2c7411f9e835e287dee
SilasReinagel <silas.reinagel@gmail.com>
2022-03-14 15:38:27 -0700

    Sound: Reactive Hooks for Slash/Stab and Throw Grenade. Closes #5364"

--- src/ProjectNeon/Assets/Features/Combatants/Animations/Sounds/CharacterAnimationSoundPublisher.cs
+++ src/ProjectNeon/Assets/Features/Combatants/Animations/Sounds/CharacterAnimationSoundPublisher.cs
@@ -6,6 +6,9 @@ public static class CharacterAnimationSoundPublisher
     public static void PlayShoot() => Play(CharacterAnimationSoundType.Shoot);
     public static void PlayRapidShot() => Play(CharacterAnimationSoundType.RapidShot);
     public static void PlayMelee() => Play(CharacterAnimationSoundType.MeleeAttack);
+    public static void PlaySlash() => Play(CharacterAnimationSoundType.Slash);
+    public static void PlayStab() => Play(CharacterAnimationSoundType.Stab);
+    public static void PlayThrowGrenade() => Play(CharacterAnimationSoundType.ThrowGrenade);
     
     public static void Play(CharacterAnimationSoundType sound)
     {
