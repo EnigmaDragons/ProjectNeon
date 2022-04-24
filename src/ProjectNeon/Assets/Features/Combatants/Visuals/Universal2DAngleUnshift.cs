529a94f3c569eef590f5d8454d4c94e252121dcc
Noah Reinagel <noah.r9@gmail.com>
2022-04-24 16:16:57 -0700

    *tuned cutscenes

--- src/ProjectNeon/Assets/Features/Combatants/Visuals/Universal2DAngleUnshift.cs
+++ src/ProjectNeon/Assets/Features/Combatants/Visuals/Universal2DAngleUnshift.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class Universal2DAngleUnshift : MonoBehaviour
 {
+    [SerializeField] private bool useLocalRotation = true;
+    
     private void Awake()
     {
-        var original = transform.rotation.eulerAngles;
-        transform.localRotation = Quaternion.Euler(-Universal2DAngleShift.angle * 2, original.y, original.z);
+        var original = useLocalRotation ? transform.localRotation.eulerAngles : transform.rotation.eulerAngles;
+        if (useLocalRotation)
+            transform.localRotation = Quaternion.Euler(-Universal2DAngleShift.angle, original.y, original.z);
+        else
+            transform.rotation = Quaternion.Euler(-Universal2DAngleShift.angle, original.y, original.z);
     }
 }
