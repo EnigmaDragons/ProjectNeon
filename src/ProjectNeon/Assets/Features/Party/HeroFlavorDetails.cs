9781e9647fa2c111ffa70c121829faf7adf9cf05
SilasReinagel <silas.reinagel@gmail.com>
2022-05-13 16:32:57 -0700

    Typo Fix: Alexei Bio - Occasionally. Closes #5786

--- src/ProjectNeon/Assets/Features/Party/HeroFlavorDetails.cs
+++ src/ProjectNeon/Assets/Features/Party/HeroFlavorDetails.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public sealed class HeroFlavorDetails
 {
     public string RoleDescription;
-    public string HeroDescription;
+    [SerializeField, TextArea(2, 2)] public string HeroDescription;
     [SerializeField, TextArea(4, 10)] public string BackStory;
 }
