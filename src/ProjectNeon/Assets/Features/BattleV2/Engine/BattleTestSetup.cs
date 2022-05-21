7ad85634b0dcff8d684c4fa95c84b13100ffafa1
SilasReinagel <silas.reinagel@gmail.com>
2022-05-21 10:04:36 -0700

    Editor: Battle Test Scene is more forgiving of enemy data errors. Closes #5805

--- src/ProjectNeon/Assets/Features/BattleV2/Engine/BattleTestSetup.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Engine/BattleTestSetup.cs
@@ -57,7 +57,7 @@ public class BattleTestSetup : MonoBehaviour
     }
 
     public void UseCustomBattlefield() => setup.InitBattleField(battlefield);
-    public void UseFixedEncounter() => state.SetNextEncounter(enemies.Select(x => x.ForStage(stage)));
+    public void UseFixedEncounter() => state.SetNextEncounter(enemies.Where(e => e != null).Select(x => x.ForStage(stage)));
     public void UseCustomEncounterSet() => setup.InitEncounterBuilder(encounterBuilder);
     public void SetupBattle() => engine.Setup();
 
