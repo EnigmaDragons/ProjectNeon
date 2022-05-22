bfd463713e090f86f883f06b78b4400ff1b6c4b2
Noah Reinagel <noah.r9@gmail.com>
2022-05-21 18:29:33 -0700

    *added battle 3 that teaches armor and resistance and magic and physical damage

---
+++ src/ProjectNeon/Assets/Features/Tutorial/Tutorial3Orchestartor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Tutorial3Orchestartor : MonoBehaviour
+{
+    private const string _callerId = "Tutorial3Orchestrator";
+
+    private void Start()
+    {
+        Message.Publish(new SetBattleUiElementVisibility(BattleUiElement.ClickableControls, false, _callerId));
+        Message.Publish(new SetBattleUiElementVisibility(BattleUiElement.TrashRecycleDropArea, false, _callerId));
+        Message.Publish(new SetBattleUiElementVisibility(BattleUiElement.PrimaryStat, false, _callerId));
+        Message.Publish(new SetBattleUiElementVisibility(BattleUiElement.PlayerShields, false, _callerId));
+    }
+}
