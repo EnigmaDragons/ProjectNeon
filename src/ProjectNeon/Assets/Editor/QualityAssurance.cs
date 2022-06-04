f02dc6d7b4dd95a5e52e6167ae311159adbe84ef
SilasReinagel <silas.reinagel@gmail.com>
2022-06-04 10:03:07 -0700

    Coding: Improved Enemy Content QA Flow. Closes #5939

--- src/ProjectNeon/Assets/Editor/QualityAssurance.cs
+++ src/ProjectNeon/Assets/Editor/QualityAssurance.cs
@@ -1,8 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
+using UnityEngine;
 
 public class QualityAssurance
 {
-    [MenuItem("Neon/Run Quality Assurance Step")]
+    private class ValidationResult
+    {
+        public string ItemName { get; }
+        public List<string> Issues { get; }
+        public bool IsValid => Issues.None();
+
+        public ValidationResult(string itemName, List<string> issues)
+        {
+            ItemName = itemName;
+            Issues = issues;
+        }
+
+        public override string ToString() => $"{ItemName} issues: {string.Join(", ", Issues)}";
+    }
+    
+    [MenuItem("Neon/QA/Run Full Content QA")]
     public static void Go()
     {
         Log.Info("QA - Started");
@@ -12,16 +30,54 @@ public class QualityAssurance
     private static void QaAllEnemies()
     {
         var enemies = ScriptableExtensions.GetAllInstances<Enemy>();
-        var numberEnemiesBad = 0;
+        var badEnemies = new List<ValidationResult>();
         var numEnemiesCount = enemies.Length;
         foreach (var e in enemies)
         {
-            if (e.IsCurrentlyWorking && !e.IsReadyForPlay)
-            {
-                Log.Error($"Broken Enemy: {e.EnemyName}");
-                ++numberEnemiesBad;
-            }
+            if (!e.IsCurrentlyWorking) continue;
+            
+            var issues = new List<string>();
+            if (!e.IsReadyForPlay) 
+                issues.Add($"Broken Enemy: {e.EnemyName} is not ReadyForPlay");
+            ValidateEnemyPrefab(e, issues);
+            if (issues.Any())
+                badEnemies.Add(new ValidationResult(e.EnemyName, issues));
         }
-        Log.Info($"QA - Enemies: {numEnemiesCount - numberEnemiesBad} out of {numEnemiesCount} are working.");
+        Log.Info($"--------------------------------------------------------------");
+        Log.Info($"QA - Enemies: {numEnemiesCount - badEnemies.Count} out of {numEnemiesCount} passed inspection.");
+        badEnemies.ForEach(e => Log.Error($"{e}"));
+        Log.Info($"--------------------------------------------------------------");
+    }
+
+    private static void ValidateEnemyPrefab(Enemy e, List<string> issues)
+    {
+        if (!e.Prefab)
+            return;
+
+        var enemyName = e.EnemyName;
+        var obj = (GameObject)PrefabUtility.InstantiatePrefab(e.Prefab);
+        
+        var stealth = obj.GetComponentInChildren<StealthTransparency>();
+        var stealth2 = obj.GetComponentInChildren<CharacterCreatorStealthTransparency>();
+        if (stealth == null && stealth2 == null) 
+            Log.Info($"{enemyName} is missing a Stealth Transparency");
+
+        if (stealth2 != null)
+            if (stealth2.viewer == null)
+                issues.Add($"{enemyName}'s Stealth Transparency Character Viewer binding is null");
+        
+        var shield = obj.GetComponentInChildren<ShieldVisual>();
+        if (shield == null) 
+            issues.Add($"{enemyName} is missing a {nameof(ShieldVisual)}");
+
+        var highlighter = obj.GetComponentInChildren<MemberHighlighter>();
+        if (highlighter == null) 
+            issues.Add($"{enemyName} is missing a {nameof(MemberHighlighter)}");
+
+        var tauntEffect = obj.GetComponentInChildren<TauntEffect>();
+        if (tauntEffect == null) 
+            issues.Add($"{enemyName} is missing a {nameof(TauntEffect)}");
+
+        Object.DestroyImmediate(obj);
     }
 }
