3cbaf3d3d8ad205dd97d471080b5bee03d1e7e26
SilasReinagel <silas.reinagel@gmail.com>
2022-06-10 16:16:26 -0700

    UI: Evolved Hero Starting Cards view. Closes #6059

--- src/ProjectNeon/Assets/Features/CardCollection/SimpleDeckCardPresenter.cs
+++ src/ProjectNeon/Assets/Features/CardCollection/SimpleDeckCardPresenter.cs
@@ -2,15 +2,20 @@ using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SimpleDeckCardPresenter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     [SerializeField] private TextMeshProUGUI cardNameText;
     [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private Image cardArt;
     [SerializeField] private HoverCard hoverCard;
+    [SerializeField] private CardCostPresenter costPresenter;
+    [SerializeField] private Image archetypeTint;
+    [SerializeField] private ArchetypeTints tints;
 
     private Canvas _canvas;
-    private Maybe<Card> _card;
+    private Maybe<Card> _card = Maybe<Card>.Missing();
     private CardTypeData _cardType;
     private int _count;
     private GameObject _hoverCard;
@@ -64,10 +69,24 @@ public class SimpleDeckCardPresenter : MonoBehaviour, IPointerEnterHandler, IPoi
     {
         cardNameText.text = _cardType.Name;
         countText.text = _count > -1 ? _count.ToString() : "";
+        if (cardArt != null)
+        {
+            cardArt.gameObject.SetActive(true);
+            cardArt.sprite = _cardType.Art;
+        }
+        if (costPresenter != null)
+        {
+            costPresenter.Render(_card, _cardType, _card.Select(c => c.Owner.PrimaryResourceType(), () => _cardType.Cost.ResourceType));
+        }
+        if (archetypeTint != null)
+            archetypeTint.color = tints.ForArchetypes(_cardType.Archetypes).WithAlpha(0.75f);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (_cardType == null)
+            return;
+        
         _hoverCard = Instantiate(hoverCard.gameObject, _canvas.transform);
         if (_card.IsPresent)
             _hoverCard.GetComponent<HoverCard>().Init(_card.Value);
