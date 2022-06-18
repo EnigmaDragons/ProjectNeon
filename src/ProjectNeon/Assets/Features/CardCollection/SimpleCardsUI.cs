75f0f38b7fbfd430e188154f3aaf404d3357ffb2
SilasReinagel <silas.reinagel@gmail.com>
2022-06-18 15:25:10 -0700

    UI: Card Zone view has different card sizes based on number of cards. Closes #6114

--- src/ProjectNeon/Assets/Features/CardCollection/SimpleCardsUI.cs
+++ src/ProjectNeon/Assets/Features/CardCollection/SimpleCardsUI.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SimpleCardsUI : MonoBehaviour
 {
-    [SerializeField] private GameObject deckParent;
+    [SerializeField] private GameObject largeZoneParent;
+    [SerializeField] private GameObject mediumZoneParent;
+    [SerializeField] private GameObject smallZoneParent;
     [SerializeField] private CardInLibraryButton cardPrototype;
     [SerializeField] private GameObject noCardsInZone;
     [SerializeField] private TextMeshProUGUI headerLabel;
@@ -12,14 +14,32 @@ public class SimpleCardsUI : MonoBehaviour
     public void Init(string header, Card[] cards)
     {
         headerLabel.text = header;
-        deckParent.DestroyAllChildren();
+        ClearZones();
         noCardsInZone.SetActive(cards.Length == 0);
+        
         var sortedCards = cards
             .OrderBy(x => x.Owner.Id)
             .ThenBy(x => x.Cost.CostSortOrder())
             .ThenBy(x => x.GetArchetypeKey())
             .ThenBy(x => x.Name);
+
+        var selectedParent = largeZoneParent;
+        if (cards.Length <= 24)
+            selectedParent = mediumZoneParent;
+        if (cards.Length <= 12)
+            selectedParent = smallZoneParent;
         foreach (var c in sortedCards)
-            Instantiate(cardPrototype, deckParent.transform).InitInfoOnly(c, () => { });
+            Instantiate(cardPrototype, selectedParent.transform).InitInfoOnly(c, () => { });
+        selectedParent.SetActive(true);
+    }
+
+    private void ClearZones()
+    {
+        largeZoneParent.DestroyAllChildren();
+        largeZoneParent.SetActive(false);
+        mediumZoneParent.DestroyAllChildren();
+        mediumZoneParent.SetActive(false);
+        smallZoneParent.DestroyAllChildren();
+        smallZoneParent.SetActive(false);
     }
 }
