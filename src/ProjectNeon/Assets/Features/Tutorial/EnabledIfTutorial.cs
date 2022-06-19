b8298b234ef85e44d28859802f27d287eca9124c
Noah Reinagel <noah.r9@gmail.com>
2022-06-18 18:04:55 -0700

    UI: now informs players if they are in a tutorial battle

---
+++ src/ProjectNeon/Assets/Features/Tutorial/EnabledIfTutorial.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class EnabledIfTutorial : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.SetActive(!CurrentAcademyData.Data.IsLicensedBenefactor);
+    }
+}
