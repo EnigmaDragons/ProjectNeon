777b644df8040b4164af66fea8b8796940e99482
SilasReinagel <silas.reinagel@gmail.com>
2022-07-07 16:38:57 -0500

    Design: Added Tutorial Hint for Gilgamesh Aegis puzzle. Closes #6260

--- src/ProjectNeon/Assets/Features/Tutorial/Tutorial11Orchestrator.cs
+++ src/ProjectNeon/Assets/Features/Tutorial/Tutorial11Orchestrator.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class Tutorial11Orchestrator : OnMessage<StartCardSetupRequested, CardResolutionFinished, WinBattleWithRewards>
+public class Tutorial11Orchestrator : OnMessage<StartCardSetupRequested, CardResolutionFinished, WinBattleWithRewards, BattleStateChanged>
 {
     private const string _callerId = "Tutorial11Orchestrator";
 
     private bool _hasPlayedAegis;
     private bool _hasWon;
+    private bool _playedAcidCoating;
+    private bool _hasShowedOutplayHint;
+    private bool _turn2AegisShown;
+    private int _turn;
     
     private void Start()
     {
@@ -27,6 +31,7 @@ public class Tutorial11Orchestrator : OnMessage<StartCardSetupRequested, CardRes
     {
         if (msg.CardName == "Acid Coating")
         {
+            _playedAcidCoating = true;
             Message.Publish(new ShowHeroBattleThought(4, "My Aegis protects me from that horrible acid you like to use"));
         }
         else if (msg.CardName == "Aegis")
@@ -42,7 +47,23 @@ public class Tutorial11Orchestrator : OnMessage<StartCardSetupRequested, CardRes
         {
             Message.Publish(new ShowHeroBattleThought(4, "A counter to my best move. Fortunately I'm still impervious"));
         }
+        else if (!_hasShowedOutplayHint && _turn == 2 && msg.CardName == "All In" && !_playedAcidCoating)
+        {
+            _hasShowedOutplayHint = true;
+            Message.Publish(new ShowHeroBattleThought(4, "Trying to prevent me from dodging, huh? That sounds like a harmful effect. My Aegis stops that!"));
+        }
     }
 
     protected override void Execute(WinBattleWithRewards msg) => _hasWon = true;
+    protected override void Execute(BattleStateChanged msg)
+    {
+        var s = msg.State;
+        _turn = s.TurnNumber;
+        var aegisCount = s.Enemies[0].Member.Aegis();
+        if (_turn == 2 && aegisCount > 0 && !_turn2AegisShown)
+        {
+            _turn2AegisShown = true;
+            Message.Publish(new ShowHeroBattleThought(4, "HAHAHA! I still have my Aegis. My victory is guaranteed!"));
+        }
+    }
 }
