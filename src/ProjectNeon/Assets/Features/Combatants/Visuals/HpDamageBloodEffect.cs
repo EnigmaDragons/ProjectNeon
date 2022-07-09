955e8e727d7b8f8364f99784470e44c129cbb1ce
SilasReinagel <silas.reinagel@gmail.com>
2022-07-09 12:21:53 -0500

    Art: Changed hp damage VFX for synthetic enemies. Closes #6278

--- src/ProjectNeon/Assets/Features/Combatants/Visuals/HpDamageBloodEffect.cs
+++ src/ProjectNeon/Assets/Features/Combatants/Visuals/HpDamageBloodEffect.cs
@@ -4,12 +4,19 @@ public class HpDamageBloodEffect : OnMessage<MemberStateChanged>
 {
     [SerializeField] private BattleState state;
     [SerializeField] private float fxDelay;
+
+    private const string OrganicDamageEffect = "HpDamage";
+    private const string MetallicDamageEffect = "HpDamageMetallic";
     
     protected override void Execute(MemberStateChanged msg)
     {
+        var damageEffectType = state.GetMaterialType(msg.MemberId()) == MemberMaterialType.Organic 
+            ? OrganicDamageEffect 
+            : MetallicDamageEffect;
+        
         if (msg.State.MissingHp() > msg.BeforeState.MissingHp)
             state.GetMaybeCenterPoint(msg.MemberId())
                 .IfPresent(cp => this.ExecuteAfterDelay(
-                    () => Message.Publish(new PlayRawBattleEffect("HpDamage", cp.position)), fxDelay));
+                    () => Message.Publish(new PlayRawBattleEffect(damageEffectType, cp.position)), fxDelay));
     }
 }
