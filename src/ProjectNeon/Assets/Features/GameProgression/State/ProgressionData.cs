0be9ce59e44783e005864b7cc7d0464cd81f91b4
SilasReinagel <silas.reinagel@gmail.com>
2022-07-10 15:21:02 -0500

    Progression: Persists Hero/Adventure Win Combinations. Closes #6307

--- src/ProjectNeon/Assets/Features/GameProgression/State/ProgressionData.cs
+++ src/ProjectNeon/Assets/Features/GameProgression/State/ProgressionData.cs
@@ -1,10 +1,33 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 [Serializable]
 public class ProgressionData
 {
     public int[] CompletedAdventureIds = new int[0];
+    public List<AdventureCompletionRecord> AdventureCompletions = new List<AdventureCompletionRecord>();
 
     public bool Completed(int adventureId) => CompletedAdventureIds.Any(a => a == adventureId);
+    public bool Completed(int adventureId, int heroId) 
+        => AdventureCompletions.Any(a => a.AdventureId == adventureId && a.HeroId == heroId);
+
+    public void Record(AdventureCompletionRecord r)
+    {
+        AdventureCompletions.Add(r);
+        AdventureCompletions = AdventureCompletions.Distinct().ToList();
+    }
+}
+
+[Serializable]
+public class AdventureCompletionRecord
+{
+    public int HeroId;
+    public int AdventureId;
+    public int Difficulty;
+    public string Version;
+
+    public bool Matches(AdventureCompletionRecord other) => other.ToString() == ToString();
+    public override string ToString() => $"{HeroId}{AdventureId}{Difficulty}{Version}";
+    public override int GetHashCode() => ToString().GetHashCode();
 }
