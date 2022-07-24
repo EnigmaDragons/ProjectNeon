eaeaa77e75da7caaf286e5b1800d21ad136a260e
SilasReinagel <silas.reinagel@gmail.com>
2022-07-24 14:32:10 -0500

    UI: Added Unlock Draft Mode Cheat. Closes #6426

--- src/ProjectNeon/Assets/Features/DraftMode/EnabledIfDraftModeIsUnlocked.cs
+++ src/ProjectNeon/Assets/Features/DraftMode/EnabledIfDraftModeIsUnlocked.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class EnabledIfDraftModeIsUnlocked : MonoBehaviour
+public class EnabledIfDraftModeIsUnlocked : OnMessage<RefreshDraftButtonAccess>
 {
     [SerializeField] private ProgressionProgress progress;
     [SerializeField] private GameObject target;
 
-    private void OnEnable() => Render();
+    protected override void AfterEnable() => Render();
+    protected override void Execute(RefreshDraftButtonAccess msg) => Render();
     private void Start() => Render();
 
     private void Render() => target.SetActive(progress.DraftModeIsUnlocked());
