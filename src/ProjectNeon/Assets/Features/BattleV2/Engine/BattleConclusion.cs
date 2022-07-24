557f8aadfa4cebc8751ed3ed609ed4d24d85ffe0
SilasReinagel <silas.reinagel@gmail.com>
2022-07-23 23:47:29 -0500

    Bug Fix: Hero damage taken wasn't persisting between Boss Phases. Fixed. Closes #6418

--- src/ProjectNeon/Assets/Features/BattleV2/Engine/BattleConclusion.cs
+++ src/ProjectNeon/Assets/Features/BattleV2/Engine/BattleConclusion.cs
@@ -19,7 +19,7 @@ public class BattleConclusion : OnMessage<BattleFinished>
         Message.Publish(new BattleRewardsStarted());
         if (adventureProgress.HasActiveAdventure && (!state.IsTutorialCombat || useNewTutorialFlow.Value))
             if (adventureProgress.AdventureProgress.IsFinalStageSegment || adventureProgress.AdventureProgress.IsFinalBoss)
-                Advance();
+                onFinished();
             else if (state.IsEliteBattle)
                 adventure.Adventure.EliteBattleRewards.GrantVictoryRewardsAndThen(onFinished, adventureProgress.AdventureProgress.CreateLootPicker(state.Party));
             else
