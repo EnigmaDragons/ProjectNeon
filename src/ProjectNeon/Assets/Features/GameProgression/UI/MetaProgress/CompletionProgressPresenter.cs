f1a9638c9ef6ed716aa41d25f45ac29214af7316
SilasReinagel <silas.reinagel@gmail.com>
2022-07-23 19:24:37 -0500

    UI: Progress Screen shows unlock requirements. Closes #6415

--- src/ProjectNeon/Assets/Features/GameProgression/UI/MetaProgress/CompletionProgressPresenter.cs
+++ src/ProjectNeon/Assets/Features/GameProgression/UI/MetaProgress/CompletionProgressPresenter.cs
@@ -9,18 +9,21 @@ public class CompletionProgressPresenter : MonoBehaviour
     [SerializeField] private TextMeshProUGUI adventureTitle;
     [SerializeField] private HeroCompletionPresenter[] heroes;
     [SerializeField] private GameObject lockedObj;
+    [SerializeField] private TextMeshProUGUI lockReasonLabel;
     [SerializeField] private GameObject completionCheckmark;
 
     public CompletionProgressPresenter Initialized(Adventure adv, ProgressionItem[] progressItems)
-        => Initialized(adv.MapTitle, adv.AdventureImage, progressItems, adv.IsLocked);
+        => Initialized(adv.MapTitle, adv.AdventureImage, progressItems, adv.IsLocked ? adv.LockConditionExplanation : Maybe<string>.Missing());
     
-    public CompletionProgressPresenter Initialized(string title, Sprite coverArt, ProgressionItem[] progressItems, bool isLocked)
+    public CompletionProgressPresenter Initialized(string title, Sprite coverArt, ProgressionItem[] progressItems, Maybe<string> lockReason)
     {
         adventureCover.sprite = coverArt;
         adventureTitle.text = title;
         RenderHeroes(progressItems);
         completionCheckmark.SetActive(progressItems.All(p => p.Completed));
-        lockedObj.SetActive(isLocked);
+        lockedObj.SetActive(lockReason.IsPresent);
+        if (lockReason.IsPresent)
+            lockReasonLabel.text = lockReason.Value;
         gameObject.SetActive(true);
         return this; 
     }
