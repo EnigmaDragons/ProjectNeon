542a11a3d8e1d16c42138ecffef15df10a42ac88
SilasReinagel <silas.reinagel@gmail.com>
2022-07-26 22:26:20 -0500

    Bug Fix: Own Aegis not longer prevents Re-Stealth. Closes #6477

--- src/ProjectNeon/Assets/Z_Tests/Features/Effects/AdjustCounterTests.cs
+++ src/ProjectNeon/Assets/Z_Tests/Features/Effects/AdjustCounterTests.cs
@@ -35,4 +35,41 @@ public class AdjustCounterTests
         
         Assert.AreEqual(0, member.State[TemporalStatType.Aegis]);
     }
+    
+    [Test]
+    public void AdjustCounterFormula_RemoveProminentWithActiveAegis_NotPrevented()
+    {
+        var member = TestMembers.Any();
+        member.State.Adjust(TemporalStatType.Aegis, 5);
+        member.State.Adjust(TemporalStatType.Prominent, 1);
+        
+        TestEffects.Apply(new EffectData
+        {
+            EffectType = EffectType.AdjustCounterFormula,
+            Formula = "-99",
+            EffectScope = new StringReference("Prominent"),
+            Unpreventable = false
+        }, member, member);
+        
+        Assert.AreEqual(0, member.State[TemporalStatType.Prominent]);
+        Assert.AreEqual(5, member.State[TemporalStatType.Aegis]);
+    }
+    
+    [Test]
+    public void AdjustCounterFormula_AddProminentWithActiveAegis_NotPrevented()
+    {
+        var member = TestMembers.Any();
+        member.State.Adjust(TemporalStatType.Aegis, 5);
+        
+        TestEffects.Apply(new EffectData
+        {
+            EffectType = EffectType.AdjustCounterFormula,
+            Formula = "1",
+            EffectScope = new StringReference("Prominent"),
+            Unpreventable = false
+        }, member, member);
+        
+        Assert.AreEqual(1, member.State[TemporalStatType.Prominent]);
+        Assert.AreEqual(5, member.State[TemporalStatType.Aegis]);
+    }
 }
