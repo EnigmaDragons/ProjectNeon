b8923a26451f678bf0584a27ffe4ca7f8b7db768
SilasReinagel <silas.reinagel@gmail.com>
2022-08-21 14:44:09 -0700

    Coding + UI: Updated Progression Tracking to Factor in Difficulties. Closes #6604

--- src/ProjectNeon/Assets/Features/GameProgression/State/ProgressionItem.cs
+++ src/ProjectNeon/Assets/Features/GameProgression/State/ProgressionItem.cs
@@ -5,6 +5,7 @@ public class ProgressionItem
     public string Description { get; }
     public Maybe<BaseHero> Hero { get; }
     public Maybe<Adventure> Adventure { get; }
+    public Maybe<Difficulty> Difficulty { get; }
 
     public string FullDescription()
     {
@@ -12,11 +13,12 @@ public class ProgressionItem
         return $"{completedWord} - {Description}";
     }
 
-    public ProgressionItem(bool completed, string description, Maybe<BaseHero> hero, Maybe<Adventure> adventure)
+    public ProgressionItem(bool completed, string description, Maybe<BaseHero> hero, Maybe<Adventure> adventure, Maybe<Difficulty> difficulty)
     {
         Completed = completed;
         Description = description;
         Hero = hero;
         Adventure = adventure;
+        Difficulty = difficulty;
     }
 }
