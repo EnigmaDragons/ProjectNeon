71514ee0d1c27a712da3d8cd5be2f66c67c31d71
Noah Reinagel <noah.r9@gmail.com>
2022-08-21 17:07:02 -0700

    *fixed difficulty unlocking and also added effects for the battles

---
+++ src/ProjectNeon/Assets/Features/Cheats/UnlockAllDifficultiesCheat.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class UnlockAllDifficultiesCheat : MonoBehaviour
+{
+    public void Unlock()
+    {
+        CurrentProgressionData.RecordCompletedAdventure(9, 99, new [] { 9 });
+    }
+}
