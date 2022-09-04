b46bd67f3ae4e229154be9d4915828165468df40
SilasReinagel <silas.reinagel@gmail.com>
2022-09-04 09:49:01 -0700

    Coding: Waits until Level Up is finished before showing breaking Newscast. Closes #6659

---
+++ src/ProjectNeon/Assets/Features/GameProgression/V5/GameSceneAutoStartTriggersV5.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneAutoStartTriggersV5 : OnMessage<PartyAdventureStateChanged, HeroLevelledUp>
+{
+    [SerializeField] private AdventureProgressV5 progress;
+    [SerializeField] private PartyAdventureState party;
+
+    private readonly HashSet<StageSegment> _triggeredSegments = new HashSet<StageSegment>();
+
+    private void Start() => TriggerNext();
+
+    protected override void Execute(PartyAdventureStateChanged msg) => TriggerNext();
+    protected override void Execute(HeroLevelledUp msg) => TriggerNext();
+
+    private void TriggerNext()
+    {  
+        foreach (var hero in party.Heroes)
+            if (hero.Levels.UnspentLevelUpPoints > 0)
+            {
+                Log.Info($"{hero.Name} - XP {hero.Levels.Xp} - Unspent Points {hero.Levels.UnspentLevelUpPoints}");
+                Message.Publish(new LevelUpHero(hero));
+                return;
+            }
+        
+        var secondary = progress.SecondarySegments;
+        for (var i = 0; i < secondary.Length; i++)
+        {
+            var segment = secondary[i];
+            if (segment.ShouldAutoStart && segment.MapNodeType == MapNodeType.Unknown && !_triggeredSegments.Contains(segment))
+            {
+                Log.Info($"V5 - Auto-Start Secondary Segment");
+                _triggeredSegments.Add(segment);
+                segment.Start();
+                return;
+            }
+        }
+    }
+}
