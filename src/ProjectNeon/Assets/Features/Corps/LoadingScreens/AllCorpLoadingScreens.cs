884bb07c8380447bf0c7cd1ef4cb4b6acabc1ee9
SilasReinagel <silas.reinagel@gmail.com>
2022-09-04 17:31:03 -0700

    UI: Added World Concept Art Gallery to Team Database. Closes #6578

--- src/ProjectNeon/Assets/Features/Corps/LoadingScreens/AllCorpLoadingScreens.cs
+++ src/ProjectNeon/Assets/Features/Corps/LoadingScreens/AllCorpLoadingScreens.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "OnlyOnce/AllCorpLoadingScreens")]
@@ -6,6 +7,8 @@ public class AllCorpLoadingScreens : ScriptableObject
     public CorpLoadingScreen[] allScreens = new CorpLoadingScreen[0]; 
     [SerializeField] private CorpLoadingScreen last;
 
+    public CorpLoadingScreen[] All => allScreens.ToArray();
+    
     public CorpLoadingScreen GetRandomNext()
     {
         var selected = allScreens.Except(last).Random(DeterministicRng.CreateRandom());
