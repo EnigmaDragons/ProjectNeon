95b19c5d4d2d2af2d54af73b2caeb4fda2a4354d
Noah Reinagel <noah.r9@gmail.com>
2022-09-15 10:02:56 -0700

    *boss progress

---
+++ src/ProjectNeon/Assets/Features/Effects/Conditions/SourceHasNoShields.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "EffectConditions/SourceHasNoShields")]
+public class SourceHasNoShields: StaticEffectCondition
+{
+    public override Maybe<string> GetShouldNotApplyReason(EffectContext ctx)
+    {
+        return ctx.Source.CurrentShield() == 0
+            ? Maybe<string>.Missing()
+            : new Maybe<string>($"{ctx.Source.Name} does have shields");
+    }
+}
