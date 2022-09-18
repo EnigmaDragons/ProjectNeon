5edc15f57674e1012fdb2bf8802d829622f46e88
SilasReinagel <silas.reinagel@gmail.com>
2022-09-18 15:29:08 -0700

    *Art: Create Cutscene-Ready Prefabs for NPCs in Anti-Robot adventure. Closes #6706

--- src/ProjectNeon/Assets/Features/Cutscenes/Segments/CutsceneSegmentData.cs
+++ src/ProjectNeon/Assets/Features/Cutscenes/Segments/CutsceneSegmentData.cs
@@ -9,6 +9,7 @@ public class CutsceneSegmentData
     public CutsceneSegmentType SegmentType;
     public StringReference DialogueCharacterId = new StringReference { UseConstant = false };
     [TextArea(4, 4)] public string Text = "";
+    
     public FloatReference FloatAmount = new FloatReference(0);
     public StoryEvent2 StoryEvent;
     public StringReference[] RequiredStates;
