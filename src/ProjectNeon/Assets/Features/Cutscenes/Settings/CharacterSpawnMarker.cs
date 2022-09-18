8db58db8d76da055dca6146233b47404d2c5ddf2
SilasReinagel <silas.reinagel@gmail.com>
2022-09-18 16:04:24 -0700

    New Cutscene: Algeron's Dilemma. Closes #6684

--- src/ProjectNeon/Assets/Features/Cutscenes/Settings/CharacterSpawnMarker.cs
+++ src/ProjectNeon/Assets/Features/Cutscenes/Settings/CharacterSpawnMarker.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 public class CharacterSpawnMarker : MonoBehaviour
 {
     [SerializeField] private StringReference characterAlias;
+    [SerializeField] private Vector3 shadowAdjustment = Vector3.zero;
 
     public string Alias => characterAlias;
 
     public GameObject SpawnTo(GameObject o)
     {
         Clear();
-        return Instantiate(o, transform);
+        var g = Instantiate(o, transform);
+        
+        if (shadowAdjustment != Vector3.zero)
+        {
+            var shadow = g.transform.Find("HeroShadow");
+            if (shadow != null)
+            {
+                var unshift = (Universal2DAngleUnshift)shadow.GetComponentInChildren(typeof(Universal2DAngleUnshift));
+                if (unshift != null)
+                    unshift.enabled = false;
+                var originalAngles = shadow.localRotation.eulerAngles;
+                shadow.localRotation = Quaternion.Euler(shadowAdjustment + originalAngles);
+            }
+        }
+
+        return g;
     }
 
     public void Clear() => transform.DestroyAllChildrenImmediate();
