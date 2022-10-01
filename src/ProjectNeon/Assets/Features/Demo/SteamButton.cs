5d56b236ecde965ae56e96cb2301c1b0628be56f
Noah Reinagel <noah.r9@gmail.com>
2022-10-01 14:08:30 -0700

    UI: ask for wishlists everywhere

---
+++ src/ProjectNeon/Assets/Features/Demo/SteamButton.cs
@@ -0,0 +1,12 @@
+﻿using Steamworks;
+using UnityEngine;
+public class SteamButton : MonoBehaviour
+{
+    public void WishlistOnSteam()
+    {
+        if (SteamManager.Initialized)
+            SteamFriends.ActivateGameOverlayToWebPage("https://store.steampowered.com/app/1412960/Metroplex_Zero_SciFi_Card_Battler/");
+        else
+            Application.OpenURL("https://store.steampowered.com/app/1412960/Metroplex_Zero/");
+    } 
+}
