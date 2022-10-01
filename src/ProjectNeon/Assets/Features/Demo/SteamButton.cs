16dbeb787996bdd41ca5dc765e5bdd2e84861a48
SilasReinagel <silas.reinagel@gmail.com>
2022-10-01 14:40:39 -0700

    Coding: Metric Published for Clicked Wishlist Button in game. Closes #6756

--- src/ProjectNeon/Assets/Features/Demo/SteamButton.cs
+++ src/ProjectNeon/Assets/Features/Demo/SteamButton.cs
@@ -4,6 +4,7 @@ public class SteamButton : MonoBehaviour
 {
     public void WishlistOnSteam()
     {
+        AllMetrics.PublishInteractedWith("Wishlist Button");
         if (SteamManager.Initialized)
             SteamFriends.ActivateGameOverlayToWebPage("https://store.steampowered.com/app/1412960/Metroplex_Zero_SciFi_Card_Battler/");
         else
