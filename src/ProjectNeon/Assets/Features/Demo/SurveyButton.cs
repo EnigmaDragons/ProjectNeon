dd9f7b42ab1f4e64f4ca1e0fe1dfd3dcc435e5de
Noah Reinagel <noah.r9@gmail.com>
2022-10-01 14:40:12 -0700

    UI: added survey button to wishlist scene

---
+++ src/ProjectNeon/Assets/Features/Demo/SurveyButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SurveyButton : MonoBehaviour
+{
+    public void GoToSurvey()
+    {
+        Application.OpenURL("https://forms.gle/SsgkqR4D7GDLmS4Y9");
+    }    
+}
