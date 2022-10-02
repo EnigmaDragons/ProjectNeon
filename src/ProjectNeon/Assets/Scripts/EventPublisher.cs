c3ebd366a33933db16d5d906934a7616edec4a6d
Noah Reinagel <noah.r9@gmail.com>
2022-10-02 13:45:49 -0700

    UI: Tab and Escape now close sub menus

--- src/ProjectNeon/Assets/Scripts/EventPublisher.cs
+++ src/ProjectNeon/Assets/Scripts/EventPublisher.cs
@@ -70,4 +70,5 @@ public class EventPublisher : ScriptableObject
     public void Proceed(string contextName) => Message.Publish(new ProceedRequested(contextName));
     public void DismissUnlockDisplay() => Message.Publish(new DismissUnlockDisplay());
     public void ShowNewscast(Cutscene cutscene) => Message.Publish(new ShowNewscast(cutscene));
+    public void CloseUiSubMenus() => Message.Publish(new CloseUiSubMenus());
 }
