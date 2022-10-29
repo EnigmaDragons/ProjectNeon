18db198b91b8475a0a86e64dd7c689a1de54a392
SilasReinagel <silas.reinagel@gmail.com>
2022-10-29 13:23:59 -0700

    Localize: Card Target Description Rule Label. Closes #6869

--- src/ProjectNeon/Assets/Features/Cards/UI/CardTargetRulePresenter.cs
+++ src/ProjectNeon/Assets/Features/Cards/UI/CardTargetRulePresenter.cs
@@ -1,50 +1,48 @@
-using TMPro;
+using I2.Loc;
 using UnityEngine;
 
 public class CardTargetRulePresenter : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI label;
+    [SerializeField] private Localize label;
 
     public void Hide() => gameObject.SetActive(false);
 
     public void Show(CardActionSequence seq) => Show(seq.Scope, seq.Group);
     public void Show(Scope scope, Group group)
     {
-        gameObject.SetActive(false);
         var text = "";
         if (group == Group.Self)
-            text = "Owner";
+            text = "Targets Owner";
         else if (scope == Scope.One && group == Group.Opponent)
-            text = "One Enemy";
+            text = "Targets One Enemy";
         else if (scope == Scope.All && group == Group.Opponent)
-            text = "All Enemies";
+            text = "Targets All Enemies";
         else if (scope == Scope.AllExcept && group == Group.Opponent)
-            text = "All Enemies Except";
+            text = "Targets All Enemies Except";
         else if (scope == Scope.AllExceptSelf && group == Group.Opponent)
-            text = "All Enemies Except Owner";
+            text = "Targets All Enemies Except Owner";
         else if (scope == Scope.One && group == Group.Ally)
-            text = "One Ally";
+            text = "Targets One Ally";
         else if (scope == Scope.All && group == Group.Ally)
-            text = "All Allies";
+            text = "Targets All Allies";
         else if (scope == Scope.AllExcept && group == Group.Ally)
-            text = "All Allies Except";
+            text = "Targets All Allies Except";
         else if (scope == Scope.AllExceptSelf && group == Group.Ally)
-            text = "All Allies Except Owner";
+            text = "Targets All Allies Except Owner";
         else if (scope == Scope.OneExceptSelf && group == Group.Ally)
-            text = "One Non-Owner Ally";
+            text = "Targets One Non-Owner Ally";
         else if (scope == Scope.All && group == Group.All)
-            text = "Everyone";
+            text = "Targets Everyone";
         else if (scope == Scope.One && group == Group.All)
-            text = "Anyone";
+            text = "Targets Anyone";
         else if (scope == Scope.OneExceptSelf && group == Group.All)
-            text = "Anyone Except Owner";
+            text = "Targets Anyone Except Owner";
         else if (scope == Scope.AllExcept && group == Group.All)
-            text = "Everyone Except";
+            text = "Targets Everyone Except";
         else if (scope == Scope.AllExceptSelf && group == Group.All)
-            text = "Everyone Except Owner";
-        
-        label.text = $"Targets {text}";
-        if (text.Length > 0)
-            gameObject.SetActive(true);
+            text = "Targets Everyone Except Owner";
+
+        label.SetTerm($"BattleUI/{text}");
+        gameObject.SetActive(text.Length > 0);
     }
 }
