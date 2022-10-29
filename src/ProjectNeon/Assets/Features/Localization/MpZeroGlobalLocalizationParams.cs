700217f484b1c1e076be8b04c4eed3e20c10cec7
SilasReinagel <silas.reinagel@gmail.com>
2022-10-29 14:05:54 -0700

    *Localization Editor Export Util

---
+++ src/ProjectNeon/Assets/Features/Localization/MpZeroGlobalLocalizationParams.cs
@@ -0,0 +1,26 @@
+using I2.Loc;
+using UnityEngine;
+
+public class MpZeroGlobalLocalizationParams : MonoBehaviour, ILocalizationParamsManager
+{
+    public void OnEnable()
+    {
+        if (LocalizationManager.ParamManagers.Contains(this)) return;
+        
+        LocalizationManager.ParamManagers.Add(this);
+        LocalizationManager.LocalizeAll(true);
+    }
+
+    public void OnDisable()
+    {
+        LocalizationManager.ParamManagers.Remove(this);
+    }
+    
+    public virtual string GetParameterValue(string paramName)
+    {
+        if (paramName == "BR")
+            return "<br>";
+
+        return null;
+    }
+}
