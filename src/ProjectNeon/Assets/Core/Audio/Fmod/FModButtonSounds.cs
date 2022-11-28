21e080f41d52e7d7b60d8f6b3732973f9b2443ee
SilasReinagel <silas.reinagel@gmail.com>
2022-11-27 18:40:09 -0700

    *Bug Fix: Button Click sound even if it will Navigate to a New Scene

--- src/ProjectNeon/Assets/Core/Audio/Fmod/FModButtonSounds.cs
+++ src/ProjectNeon/Assets/Core/Audio/Fmod/FModButtonSounds.cs
@@ -8,15 +8,15 @@ public class FModButtonSounds : OnMessage<NavigateToSceneRequested>
     [SerializeField, FMODUnity.EventRef] private string hoverEnterSound;
     [SerializeField, FMODUnity.EventRef] private string hoverExitSound;
 
-    public void OnClick() => Play(useBigClickSound ? bigClickSound : standardClickSound);
+    public void OnClick() => Play(useBigClickSound ? bigClickSound : standardClickSound, true);
     public void OnHoverEnter() => Play(hoverEnterSound);
     public void OnHoverExit() => Play(hoverExitSound);
 
     public void SetAsPrimary() => useBigClickSound = true;
     
-    private void Play(string sound)
+    private void Play(string sound, bool playEvenIfNotEnabled = false)
     {
-        if (enabled && !string.IsNullOrWhiteSpace(sound))
+        if ((enabled || playEvenIfNotEnabled) && !string.IsNullOrWhiteSpace(sound))
             FMODUnity.RuntimeManager.PlayOneShotAttached(sound, gameObject);
     }
 
