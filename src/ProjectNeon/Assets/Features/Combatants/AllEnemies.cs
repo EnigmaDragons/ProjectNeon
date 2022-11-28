dbf7a4915ec0c96cffd14577d1a8077bd57763af
SilasReinagel <silas.reinagel@gmail.com>
2022-11-27 18:30:59 -0700

    Project: Update Banner Art for v0.0.56 Closes #7005

--- src/ProjectNeon/Assets/Features/Combatants/AllEnemies.cs
+++ src/ProjectNeon/Assets/Features/Combatants/AllEnemies.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "OnlyOnce/All Enemies")]
 public class AllEnemies : ScriptableObject
 {
-    private Dictionary<int, Enemy> _map;
     [UnityEngine.UI.Extensions.ReadOnly] public Enemy[] Enemies; //Unity Collection Readonly
 
-    public Dictionary<int, Enemy> GetMap() => _map ??= Enemies.ToDictionary(x => x.id, x => x);
+    public Dictionary<int, Enemy> GetMap() => Enemies.ToDictionary(x => x.id, x => x);
     public Maybe<Enemy> GetEnemyById(int id) => GetMap().ValueOrMaybe(id);
 }
