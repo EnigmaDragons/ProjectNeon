53bb41fe1ce0693f31d854d27558bd621f761482
ZavixDragon <noah.r9@gmail.com>
2023-02-18 15:07:30 -0700

    *changed draw archetype cards to include discard and added unhighlights for them

---
+++ src/ProjectNeon/Assets/Features/Cards/Conditions/NoCardCanBeDrawnOfArchetype.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "CardConditions/NoCardCanBeDrawnOfArchetype")]
+public class NoCardCanBeDrawnOfArchetype : StaticCardCondition
+{
+    [SerializeField] private CardPlayZones zones;
+    [SerializeField] private StringVariable archetype;
+    
+    public override bool ConditionMet(CardConditionContext ctx)
+        => zones.DrawZone.Cards.None(x => x.Archetypes.Contains(archetype.Value)) 
+           && zones.DiscardZone.Cards.None(x => x.Archetypes.Contains(archetype.Value));
+    
+    public override string Description => $"No card can be drawn of type {archetype.Value}, all of them are in your hand";
+}
--- src/ProjectNeon/Assets/Features/Effects/CardEffects/ChooseAndDrawCardOfArchetype.cs
+++ src/ProjectNeon/Assets/Features/Effects/CardEffects/ChooseAndDrawCardOfArchetype.cs
@@ -14,13 +14,13 @@ public class ChooseAndDrawCardOfArchetype : Effect
     {
         var possibleCards = new List<Card>();
         possibleCards.AddRange(ctx.PlayerCardZones.DrawZone.Cards.Where(x => x.Archetypes.Contains(archetype)));
-        //possibleCards.AddRange(ctx.PlayerCardZones.DiscardZone.Cards.Where(x => x.Archetypes.Contains(archetype)));
+        possibleCards.AddRange(ctx.PlayerCardZones.DiscardZone.Cards.Where(x => x.Archetypes.Contains(archetype)));
         ctx.Selections.CardSelectionOptions = possibleCards.Shuffled().ToArray();
         ctx.Selections.OnCardSelected = card =>
         {
             if (ctx.PlayerCardZones.DiscardZone.Cards.Contains(card))
                 ctx.PlayerCardZones.DiscardZone.Remove(card);
-            if (ctx.PlayerCardZones.DrawZone.Cards.Contains(card))
+            else if (ctx.PlayerCardZones.DrawZone.Cards.Contains(card))
                 ctx.PlayerCardZones.DrawZone.Remove(card);
             ctx.PlayerCardZones.HandZone.PutOnBottom(card);
         };
