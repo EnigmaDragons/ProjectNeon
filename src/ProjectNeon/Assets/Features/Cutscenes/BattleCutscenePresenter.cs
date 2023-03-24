7458c8bad65e244a49576d2fabbd2e08cf7405a5
Noah Reinagel <noah.r9@gmail.com>
2023-03-24 11:45:05 -0700

    *upgraded battle cutscene presenter to give us more data if it breaks again

--- src/ProjectNeon/Assets/Features/Cutscenes/BattleCutscenePresenter.cs
+++ src/ProjectNeon/Assets/Features/Cutscenes/BattleCutscenePresenter.cs
@@ -17,40 +17,65 @@ public class BattleCutscenePresenter : BaseCutscenePresenter
 
     public IEnumerator Begin()
     {
-        if (cutscene.StartBattleCutscene == null)
+        if (cutscene.StartBattleCutscene == null || cutscene.StartBattleCutscene.Segments == null)
         {
-            Log.Error("Tried to enable BattleCutscenePresenter without a Battle Cutscene");
+            Log.Error("Tried to enable BattleCutscenePresenter without a Battle Cutscene or possibly segments");
             Message.Publish(new CutsceneFinished());
             yield break;
         }
         
         Message.Publish(new SetBattleUiElementVisibility(BattleUiElement.EnemyInfo, false, _callerId));
+        SetGameObjectsActiveState();
+        SetupCharactersList();
+        InitHeroesForCutscene();
+        InitEnemiesForCutscene();
+
+        DebugLog($"Num Cutscene Segments {cutscene.StartBattleCutscene.Segments.Length}");
+        yield return new WaitForSeconds(1f);
+        StartCutsceneMessageGroup();
+    }
+
+    private void SetGameObjectsActiveState()
+    {
         disableOnStarted.ForEach(d => d.SetActive(false));
         enableOnStarted.ForEach(d => d.SetActive(true));
+    }
+
+    private void SetupCharactersList()
+    {
         Characters.Clear();
         Characters.Add(narrator);
         Characters.Add(you);
         Characters.Add(player);
+    }
+
+    private void InitHeroesForCutscene()
+    {
         var heroes = heroArea.GetComponentsInChildren<CutsceneCharacter>();
         for (var i = 0; i < heroes.Length && i < heroAliases.Length; i++)
         {
             heroes[i].Init(heroAliases[i]);
             Characters.Add(heroes[i]);
         }
+    }
+
+    private void InitEnemiesForCutscene()
+    {
         var enemies = enemyArea.GetComponentsInChildren<CutsceneCharacter>();
         for (var i = 0; i < enemies.Length; i++)
         {
             enemies[i].Init($"enemy{i+1}");
             Characters.Add(enemies[i]);
         }
-        
-        DebugLog($"Num Cutscene Segments {cutscene.StartBattleCutscene.Segments.Length}");
-        yield return new WaitForSeconds(1f);
+    }
+
+    private void StartCutsceneMessageGroup()
+    {
         MessageGroup.Start(
             new MultiplePayloads("Cutscene Script", cutscene.StartBattleCutscene.Segments.Select(s => new ShowCutsceneSegment(s)).Cast<object>().ToArray()), 
             () => Message.Publish(new CutsceneFinished()));
     }
-    
+
     protected override void Execute(SkipCutsceneRequested msg) => FinishCutscene();
 
     protected override void FinishCutscene()
