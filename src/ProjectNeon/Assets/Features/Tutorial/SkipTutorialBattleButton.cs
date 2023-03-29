7e0cd1bb488e62c9b7ea35272331546623245101
Noah Reinagel <noah.r9@gmail.com>
2023-03-29 07:42:41 -0700

    Bug Fix: race condition with mp zero global params and localization & Skip Tutorial button with warning dialogs have been added where apropriate

---
+++ src/ProjectNeon/Assets/Features/Tutorial/SkipTutorialBattleButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class SkipTutorialBattleButton : MonoBehaviour, ILocalizeTerms
+{
+    [SerializeField] private LocalizedCommandButton button;
+    [SerializeField] private BattleState battleState;
+    [SerializeField] private CurrentAdventure adventure;
+    [SerializeField] private EventPublisher eventPublisher;
+
+    private string Term => "Tutorials/SkipTutorial";
+    
+    private void Start()
+    {
+        button.gameObject.SetActive(adventure.Adventure.Id == AdventureIds.TutorialAdventureId || battleState.IsSingleTutorialBattle);
+        if (battleState.IsSingleTutorialBattle)
+            button.InitTerm(Term, () => eventPublisher.WinBattle());
+        else if (adventure.Adventure.Id == AdventureIds.TutorialAdventureId)
+            button.InitTerm(Term, () => Message.Publish(new ShowLocalizedDialog
+            {
+                PrimaryAction = () => eventPublisher.WinBattle(),
+                PrimaryButtonTerm = DialogTerms.OptionSkipBattle,
+                SecondaryAction = () => {},
+                SecondaryButtonTerm = DialogTerms.OptionOops,
+                PromptTerm = DialogTerms.SkipTutorialBattleWarning
+            }));
+    }
+
+    public string[] GetLocalizeTerms()
+        => new[] {Term, DialogTerms.OptionSkipBattle, DialogTerms.OptionOops, DialogTerms.SkipTutorialBattleWarning};
+}
