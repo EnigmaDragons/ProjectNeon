ef3627c2ec5e6e78e84680b3d33d91e9d160c16b
Noah Reinagel <noah.r9@gmail.com>
2023-03-29 21:00:31 -0700

    Bug Fix: rule tooltips not hiding when their subview is disabled

--- src/ProjectNeon/Assets/Features/Rules/RuleUiTooltip.cs
+++ src/ProjectNeon/Assets/Features/Rules/RuleUiTooltip.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -6,13 +7,31 @@ public class RuleUiTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     [SerializeField] private StringVariable key;
     [SerializeField] private StringKeyTermCollection rules;
 
+    private bool _showing;
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (rules != null && key != null)
+        {
+            _showing = true;
             Message.Publish(new ShowTooltip(transform.position, rules[key].ToLocalized(), showBackground: true));
+        }
     }
 
-    public void OnPointerExit(PointerEventData eventData) => Message.Publish(new HideTooltip());
+    private void Hide()
+    {
+        if (_showing)
+        {
+            Message.Publish(new HideTooltip());
+            _showing = false;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData) => Hide();
+
+    private void OnDisable() => Hide();
+    
+    private void OnDestroy() => Hide();
 
     public string[] GetLocalizeTerms()
         => new [] { rules[key] };
