85346c124f30b8a9ff1bbd721d721369b5f6276f
Noah Reinagel <noah.r9@gmail.com>
2023-03-31 11:24:32 -0700

    *beginning to boxes

---
+++ src/ProjectNeon/Assets/Features/SerializableBoxes/CardTypeDataBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+[Serializable] 
+public class CardTypeDataBox
+{
+    public Card Card;
+    public CardType CardType;
+    public ReactionCardType ReactionCardType;
+    
+    public CardTypeData Get()
+    {
+        
+    }
+    
+    public void Set(CardTypeData cardTypeData)
+    {
+        if (cardTypeData == null)
+            return;
+        Card = null;
+        CardType = null;
+        ReactionCardType = null;
+        if (cardTypeData is Card card)
+            Card = card;
+        else if ()
+    }
+}
