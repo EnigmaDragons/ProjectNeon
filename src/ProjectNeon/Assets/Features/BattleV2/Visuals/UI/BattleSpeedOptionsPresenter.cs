c38451825ecf8d690dff78d41f0de79386bba78a
Noah Reinagel <noah.r9@gmail.com>
2023-08-25 16:21:08 -0700

    *added battle speed controls to settings for controller players

---
+++ src/ProjectNeon/Assets/Features/BattleV2/Visuals/UI/BattleSpeedOptionsPresenter.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BattleSpeedOptionsPresenter : MonoBehaviour
+{
+    [SerializeField] private Button normal;
+    [SerializeField] private Button fast;
+    [SerializeField] private Button faster;
+    [SerializeField] private string[] battleSceneNames;
+
+    public int CurrentFactor => CurrentGameOptions.Data.BattleSpeedFactor;
+    
+    private void Awake()
+    {
+        normal.onClick.AddListener(() => SetFactor(1));
+        fast.onClick.AddListener(() => SetFactor(2));
+        faster.onClick.AddListener(() => SetFactor(4));
+        SetFactor(CurrentFactor);
+    }
+    
+    private void SetFactor(int factor)
+    {
+        normal.interactable = factor != 1;
+        fast.interactable = factor != 2;
+        faster.interactable = factor != 4;
+        if (battleSceneNames.Contains(SceneManager.GetActiveScene().name))
+            Time.timeScale = factor;
+        if (factor != CurrentFactor)
+            Message.Publish(new BattleSpeedChanged(factor)); 
+        CurrentGameOptions.SetBattleSpeed(factor);
+    }
+}
