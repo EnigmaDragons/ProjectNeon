11f9923e5103f80850abbba6cb541ff043772c01
Noah Reinagel <noah.r9@gmail.com>
2024-01-18 13:15:47 -0700

    Bug Fix: Trickster no longer appears if you have never seen him

--- src/ProjectNeon/Assets/Features/Bosses/BossSelectionPresenter.cs
+++ src/ProjectNeon/Assets/Features/Bosses/BossSelectionPresenter.cs
@@ -1,4 +1,5 @@
-﻿using I2.Loc;
+﻿using System.Linq;
+using I2.Loc;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,18 +10,27 @@ public class BossSelectionPresenter : MonoBehaviour
     [SerializeField] private Localize description;
     [SerializeField] private AllBosses bosses;
 
+    private Boss[] _bossesAvailable;
+
     private int _index;
     
     public Boss SelectedBoss { get; private set; }
 
-    public void Next() => RenderBoss(_index == bosses.bosses.Length - 1 ? 0 : _index + 1);
-    
-    private void Awake() => RenderBoss(Rng.Int(0, bosses.bosses.Length));
+    public void Next() => RenderBoss(_index == _bossesAvailable.Length - 1 ? 0 : _index + 1);
+
+    private void Awake()
+    {
+        if (CurrentProgressionData.Data.HasSeenAlgeronFinalBoss)
+            _bossesAvailable = bosses.bosses;
+        else
+            _bossesAvailable = bosses.bosses.Where(x => x.id != 2).ToArray(); //algeron's id
+        RenderBoss(Rng.Int(0, _bossesAvailable.Length));
+    }
 
     private void RenderBoss(int index)
     {
         _index = index;
-        SelectedBoss = bosses.bosses[_index];
+        SelectedBoss = _bossesAvailable[_index];
         bust.sprite = SelectedBoss.Bust;
         name.SetTerm(SelectedBoss.NameTerm);
         description.SetTerm(SelectedBoss.DescriptionTerm);
